cd582bfa0b1ed7fe7503d694a9a0efd744007ce4
mingji <fengmingxiao.fmx@alibaba-inc.com>
2022-06-17 19:01:10 +0800

    [BUG]Fix worker lost in HA mode when leader lost.

--- common/src/main/scala/com/aliyun/emr/rss/common/util/RpcUtils.scala
+++ common/src/main/scala/com/aliyun/emr/rss/common/util/RpcUtils.scala
@@ -32,6 +32,11 @@ private[rss] object RpcUtils {
     conf.getTimeAsMs("rss.rpc.retry.wait", "3s")
   }
 
+  def haClientAskRpcTimeout(conf: RssConf): RpcTimeout = {
+    RpcTimeout(conf, Seq("rss.haclient.rpc.askTimeout",
+      "rss.network.timeout"), "30s")
+  }
+
   /** Returns the default Spark timeout to use for RPC ask operations. */
   def askRpcTimeout(conf: RssConf): RpcTimeout = {
     RpcTimeout(conf, Seq("rss.rpc.askTimeout",
@@ -41,7 +46,7 @@ private[rss] object RpcUtils {
   /** Returns the default Spark timeout to use for RPC remote endpoint lookup. */
   def lookupRpcTimeout(conf: RssConf): RpcTimeout = {
     RpcTimeout(conf, Seq("rss.rpc.lookupTimeout",
-      "rss.network.timeout"), "240s")
+      "rss.network.timeout"), "30s")
   }
 
   private val MAX_MESSAGE_SIZE_IN_MB = Int.MaxValue / 1024 / 1024
