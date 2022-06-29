5236dccb4fb5cf7540df7112df76b41890a76272
Angerszhuuuu <angers.zhu@gmail.com>
2022-06-29 18:12:35 +0800

    upfate

--- server-master/src/main/scala/com/aliyun/emr/rss/service/deploy/master/Master.scala
+++ server-master/src/main/scala/com/aliyun/emr/rss/service/deploy/master/Master.scala
@@ -255,11 +255,11 @@ private[deploy] class Master(
       requestId: String): Unit = {
     val targetWorker = new WorkerInfo(host, rpcPort, pushPort, fetchPort, replicatePort,
       -1, null)
-    val worker: WorkerInfo = workersSnapShot
+    val registered = workersSnapShot
       .asScala
       .find(_ == targetWorker)
-      .orNull
-    if (worker == null) {
+      .isDefined
+    if (registered) {
       logWarning(s"Received heartbeat from unknown worker " +
         s"$host:$rpcPort:$pushPort:$fetchPort:$replicatePort.")
     } else {
@@ -273,7 +273,7 @@ private[deploy] class Master(
         expiredShuffleKeys.add(shuffleKey)
       }
     }
-    context.reply(HeartbeatResponse(expiredShuffleKeys))
+    context.reply(HeartbeatResponse(expiredShuffleKeys, registered))
   }
 
   private def handleWorkerLost(context: RpcCallContext, host: String, rpcPort: Int, pushPort: Int,
