bd68a756e819537a171dc03107ff00e55e6b4922
Angerszhuuuu <angers.zhu@gmail.com>
2022-07-08 14:03:56 +0800

    [ISSUE-222][BUG] GetBlacklist/GetBlacklistResponse should replace null value with empty list

--- client/src/main/scala/com/aliyun/emr/rss/client/write/LifecycleManager.scala
+++ client/src/main/scala/com/aliyun/emr/rss/client/write/LifecycleManager.scala
@@ -1075,16 +1075,16 @@ class LifecycleManager(appId: String, val conf: RssConf) extends RpcEndpoint wit
 
   private def handleGetBlacklist(msg: GetBlacklist): Unit = {
     val res = requestGetBlacklist(rssHARetryClient, msg)
-    if (res.statusCode == StatusCode.Success) {
-      logInfo(s"Received Blacklist from Master, blacklist: ${res.blacklist} " +
-        s"unkown workers: ${res.unknownWorkers}")
-      blacklist.clear()
-      if (res.blacklist != null) {
+    res.statusCode match {
+      case StatusCode.Success =>
+        logInfo(s"Received Blacklist from Master, blacklist: ${res.blacklist} " +
+          s"unkown workers: ${res.unknownWorkers}")
+        blacklist.clear()
         blacklist.addAll(res.blacklist)
-      }
-      if (res.unknownWorkers != null) {
         blacklist.addAll(res.unknownWorkers)
-      }
+      case StatusCode.Failed =>
+        logError(s"GetBlacklist RPC request failed!")
+      case _ => // won't happen
     }
   }
 
@@ -1171,14 +1171,15 @@ class LifecycleManager(appId: String, val conf: RssConf) extends RpcEndpoint wit
     }
   }
 
-  private def requestGetBlacklist(rssHARetryClient: RssHARetryClient,
-    msg: GetBlacklist): GetBlacklistResponse = {
+  private def requestGetBlacklist(
+      rssHARetryClient: RssHARetryClient,
+      msg: GetBlacklist): GetBlacklistResponse = {
     try {
       rssHARetryClient.askSync[GetBlacklistResponse](msg, classOf[GetBlacklistResponse])
     } catch {
       case e: Exception =>
         logError(s"AskSync GetBlacklist failed.", e)
-        GetBlacklistResponse(StatusCode.Failed, null, null)
+        GetBlacklistResponse(StatusCode.Failed, List.empty.asJava, List.empty.asJava)
     }
   }
 
