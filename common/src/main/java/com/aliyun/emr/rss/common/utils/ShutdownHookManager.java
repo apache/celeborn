97d781e43b2a871b00fbcf6d217a0e5b4ff91c23
Angerszhuuuu <angers.zhu@gmail.com>
2022-08-11 18:48:48 +0800

    Update ShutdownHookManager.java

--- common/src/main/java/com/aliyun/emr/rss/common/utils/ShutdownHookManager.java
+++ common/src/main/java/com/aliyun/emr/rss/common/utils/ShutdownHookManager.java
@@ -26,8 +26,8 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
