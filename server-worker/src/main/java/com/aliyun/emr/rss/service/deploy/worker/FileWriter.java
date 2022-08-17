e8f3d71c07c8ce4e6c623469333193dbd5577ee4
Angerszhuuuu <angers.zhu@gmail.com>
2022-08-17 11:11:28 +0800

    。。

--- server-worker/src/main/java/com/aliyun/emr/rss/service/deploy/worker/FileWriter.java
+++ server-worker/src/main/java/com/aliyun/emr/rss/service/deploy/worker/FileWriter.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
--- server-worker/src/test/java/com/aliyun/emr/rss/service/deploy/worker/PartitionFilesSorterSuiteJ.java
+++ server-worker/src/test/java/com/aliyun/emr/rss/service/deploy/worker/PartitionFilesSorterSuiteJ.java
@@ -22,7 +22,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
