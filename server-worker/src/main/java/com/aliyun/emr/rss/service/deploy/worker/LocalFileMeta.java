c75927105e9e048689d9c87a163130b8ceb452d4
Angerszhuuuu <angers.zhu@gmail.com>
2022-08-17 18:01:05 +0800

    Update LocalFileMeta.java

--- server-worker/src/main/java/com/aliyun/emr/rss/service/deploy/worker/LocalFileMeta.java
+++ server-worker/src/main/java/com/aliyun/emr/rss/service/deploy/worker/LocalFileMeta.java
@@ -20,9 +20,6 @@ package com.aliyun.emr.rss.service.deploy.worker;
 import java.io.File;
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class LocalFileMeta {
   String shuffleKey;
   String fileName;
@@ -30,13 +27,12 @@ public class LocalFileMeta {
   long bytesFlushed = 0;
   ArrayList<Long> chunkOffsets = new ArrayList<>();
 
-  @JsonCreator
   public LocalFileMeta(
-     @JsonProperty("shuffleKey") String shuffleKey,
-     @JsonProperty("fileName") String fileName,
-     @JsonProperty("file") File file,
-     @JsonProperty("bytesFlushed") long bytesFlushed,
-     @JsonProperty("chunkOffsets") ArrayList<Long> chunkOffsets) {
+     String shuffleKey,
+     String fileName,
+     File file,
+     long bytesFlushed,
+     ArrayList<Long> chunkOffsets) {
     this.shuffleKey = shuffleKey;
     this.fileName = fileName;
     this.file = file;
