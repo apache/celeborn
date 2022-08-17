3d8e86f80f0090ded59c7263951242042dc0b4f3
Angerszhuuuu <angers.zhu@gmail.com>
2022-08-17 16:32:21 +0800

    Update Worker.scala

--- server-worker/src/main/scala/com/aliyun/emr/rss/service/deploy/worker/Worker.scala
+++ server-worker/src/main/scala/com/aliyun/emr/rss/service/deploy/worker/Worker.scala
@@ -203,7 +203,7 @@ private[deploy] class Worker(
         case e: Throwable =>
           logError("Re-register worker failed after worker lost.", e)
           // Register failed then stop server
-          controller.stop()
+          stop()
       }
     }
   }
