d845504c1e230b8eb575525bcecfbf7fb8d0729e
liugs0213 <55676558+liugs0213@users.noreply.github.com>
2022-08-22 22:59:44 +0800

    Update DeviceMonitor.scala

--- server-worker/src/main/scala/com/aliyun/emr/rss/service/deploy/worker/DeviceMonitor.scala
+++ server-worker/src/main/scala/com/aliyun/emr/rss/service/deploy/worker/DeviceMonitor.scala
@@ -223,7 +223,7 @@ class LocalDeviceMonitor(
                 if (checkDiskUsage && DeviceMonitor.highDiskUsage(essConf, diskInfo.mountPoint)) {
                   logger.error(s"${diskInfo.mountPoint} high_disk_usage error, notify observers")
                   device.notifyObserversOnHighDiskUsage(diskInfo.mountPoint)
-                } else if (checkReadWrite && 
+                } else if (checkReadWrite &&
                 DeviceMonitor.readWriteError(essConf, diskInfo.dirs.head)) {
                   logger.error(s"${diskInfo.mountPoint} read-write error, notify observers")
                   // We think that if one dir in device has read-write problem, if possible all
