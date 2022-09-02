88af816034fe2b8fcd9f6c271f7a85a50df1eee3
Angerszhuuuu <angers.zhu@gmail.com>
2022-09-02 10:40:24 +0800

    Update Master.scala

--- server-master/src/main/scala/com/aliyun/emr/rss/service/deploy/master/Master.scala
+++ server-master/src/main/scala/com/aliyun/emr/rss/service/deploy/master/Master.scala
@@ -22,6 +22,10 @@ import java.net.BindException
 import java.util
 import java.util.concurrent.{ScheduledFuture, TimeUnit}
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.util.Random
+
 import com.aliyun.emr.rss.common.RssConf
 import com.aliyun.emr.rss.common.RssConf.haEnabled
 import com.aliyun.emr.rss.common.haclient.RssHARetryClient
