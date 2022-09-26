2ac783e897b6024cc5bf443ade6679aeeb70764f
mingji <fengmingxiao.fmx@alibaba-inc.com>
2022-09-26 12:54:28 +0800

    fix license.

--- common/src/main/scala/com/aliyun/emr/rss/common/protocol/message/Message.scala
+++ common/src/main/scala/com/aliyun/emr/rss/common/protocol/message/Message.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.aliyun.emr.rss.common.protocol.message
 
 import scala.collection.JavaConverters._
@@ -11,6 +28,9 @@ import com.aliyun.emr.rss.common.util.Utils
 trait Message extends Serializable {
   def toTransportMessage: TransportMessage = {
     this match {
+      case _: PbCheckForWorkerTimeoutOrBuilder =>
+        new TransportMessage(MessageType.CHECK_FOR_WORKER_TIMEOUT, null)
+
       case CheckForApplicationTimeOut =>
         new TransportMessage(MessageType.CHECK_FOR_APPLICATION_TIMEOUT, null)
 
