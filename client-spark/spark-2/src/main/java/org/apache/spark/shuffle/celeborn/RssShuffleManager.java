62b697f7b2342adab3019b3552ce345c08aec84d
mingji <fengmingxiao.fmx@alibaba-inc.com>
2022-10-08 16:49:14 +0800

    update for spark2.x

--- client-spark/spark-2/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
+++ client-spark/spark-2/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
@@ -30,7 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.celeborn.client.ShuffleClient;
-import org.apache.celeborn.client.write.LifecycleManager;
+import org.apache.celeborn.client.LifecycleManager;
 import org.apache.celeborn.common.RssConf;
 
 public class RssShuffleManager implements ShuffleManager {
