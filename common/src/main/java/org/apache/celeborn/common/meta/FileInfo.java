a34ec37f4aa73822c094830acc2ae9b15b729298
zhongqiang.czq <zhongqiang.czq@alibaba-inc.com>
2022-12-13 16:21:48 +0800

    [CELEBORN-126] Fileinfo adds member bufferSize

--- common/src/main/java/org/apache/celeborn/common/meta/FileInfo.java
+++ common/src/main/java/org/apache/celeborn/common/meta/FileInfo.java
@@ -37,6 +37,7 @@ public class FileInfo {
   private final List<Long> chunkOffsets;
   private final UserIdentifier userIdentifier;
   private final PartitionType partitionType;
+  private int bufferSize;
 
   public FileInfo(String filePath, List<Long> chunkOffsets, UserIdentifier userIdentifier) {
     this(filePath, chunkOffsets, userIdentifier, PartitionType.REDUCE);
@@ -163,5 +164,9 @@ public class FileInfo {
         + ", partitionType="
         + partitionType
         + '}';
+  }
+
+  public void setBufferSize(int bufferSize) {
+    this.bufferSize = bufferSize;
   }
 }
