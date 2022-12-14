0abaa16c5d902ffaee6583a88975a554279daf1e
nafiy <nafiyaix97@gmail.com>
2022-12-14 15:39:59 +0800

    [CELEBORN-139][BUG] Fix read wrong yaml file format when loading config

--- common/src/main/scala/org/apache/celeborn/common/util/Utils.scala
+++ common/src/main/scala/org/apache/celeborn/common/util/Utils.scala
@@ -561,7 +561,7 @@ object Utils extends Logging {
   def getDefaultQuotaConfigurationFile(env: Map[String, String] = sys.env): String = {
     env.get("CELEBORN_CONF_DIR")
       .orElse(env.get("CELEBORN_HOME").map { t => s"$t${File.separator}conf" })
-      .map { t => new File(s"$t${File.separator}quota.yml") }
+      .map { t => new File(s"$t${File.separator}quota.yaml") }
       .filter(_.isFile)
       .map(_.getAbsolutePath)
       .orNull
