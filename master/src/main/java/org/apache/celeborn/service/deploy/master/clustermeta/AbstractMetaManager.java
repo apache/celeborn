99461e4b6c0783380387e0281e0fa4e929c7add3
Angerszhuuuu <angers.zhu@gmail.com>
2022-12-28 15:42:11 +0800

    Update AbstractMetaManager.java

--- master/src/main/java/org/apache/celeborn/service/deploy/master/clustermeta/AbstractMetaManager.java
+++ master/src/main/java/org/apache/celeborn/service/deploy/master/clustermeta/AbstractMetaManager.java
@@ -21,7 +21,6 @@ import static org.apache.celeborn.common.protocol.RpcNameConstants.WORKER_EP;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
