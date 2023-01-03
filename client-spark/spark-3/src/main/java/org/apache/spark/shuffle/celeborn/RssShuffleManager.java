41b1fa46d33b993dcd911fea77e4dc435a017b62
jxysoft <xianyao.jiang@gmail.com>
2023-01-03 14:28:09 +0800

    [CELEBORN-185][SPARK] Can't release shuffle data if rss fallback to nss (#1133)
    Co-authored-by: xianyao.jiang <xianyao.jiang@antfin.com>

--- client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
+++ client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
@@ -170,6 +170,7 @@ public class RssShuffleManager implements ShuffleManager {
               "Unrecognized shuffle write mode!" + celebornConf.shuffleWriterMode());
         }
       } else {
+        sortShuffleIds.add(handle.shuffleId());
         return sortShuffleManager().getWriter(handle, mapId, context, metrics);
       }
     } catch (IOException e) {
