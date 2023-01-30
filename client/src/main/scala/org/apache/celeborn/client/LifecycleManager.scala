ed74d1f2c04bac7696b96ee15f78e98f88ac4db8
Angerszhuuuu <angers.zhu@gmail.com>
2023-01-30 16:21:01 +0800

    Update LifecycleManager.scala

--- client/src/main/scala/org/apache/celeborn/client/LifecycleManager.scala
+++ client/src/main/scala/org/apache/celeborn/client/LifecycleManager.scala
@@ -535,7 +535,7 @@ class LifecycleManager(appId: String, val conf: CelebornConf) extends RpcEndpoin
           blacklistPartitionWorker(oldPartition.getPeer, StatusCode.PUSH_DATA_FAIL_SLAVE)
         case StatusCode.PUSH_DATA_TIMEOUT =>
           blacklistPartitionWorker(oldPartition, StatusCode.PUSH_DATA_TIMEOUT)
-          if (oldPartition.getPeer != null) {
+          if (oldPartition.getPeer != null && conf.blacklistSlaveEnabled) {
             blacklistPartitionWorker(oldPartition.getPeer, StatusCode.PUSH_DATA_TIMEOUT)
           }
         case _ =>
