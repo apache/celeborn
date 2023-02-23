ad08306e795452ebd8308ff3db14954cf7875f71
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2023-02-23 23:42:50 +0800

    refine ut

--- tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/RssSortSuite.scala
+++ tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/RssSortSuite.scala
@@ -65,9 +65,8 @@ class RssSortSuite extends AnyFunSuite
 
   test("celeborn spark integration test - pipeline sort") {
     val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[4]")
-      .set("spark.celeborn.push.sortMemory.threshold", "1m")
-      .set("spark.celeborn.push.pipeline.enabled", "true")
-      .set("spark.celeborn.push.randomizePartitionId.enabled", "true")
+      .set("spark.celeborn.push.sort.pipeline.enabled", "true")
+      .set("spark.celeborn.push.sort.randomizePartitionId.enabled", "true")
     val ss = SparkSession.builder().config(updateSparkConf(sparkConf, true)).getOrCreate()
     val value = Range(1, 10000).mkString(",")
     val tuples = ss.sparkContext.parallelize(1 to 10000, 2)
