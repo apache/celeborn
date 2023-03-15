6f47a4397e40a88111afebfb2dce91ebb3b76040
Angerszhuuuu <angers.zhu@gmail.com>
2023-03-15 16:11:23 +0800

    Update

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Worker.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Worker.scala
@@ -512,7 +512,7 @@ private[celeborn] class Worker(
     val sb = new StringBuilder
     sb.append("==================== Unavailable Peers of Worker =====================\n")
     unavailablePeers.asScala.foreach { case (peer, time) =>
-      sb.append(s"${peer.toUniqueId().padTo(50, " ").mkString("")}$time\n")
+      sb.append(s"${peer.toUniqueId().padTo(50, " ").mkString}$time\n")
     }
     sb.toString()
   }
