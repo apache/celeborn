4f41383d1568efde2c19f392d36e6ced99ded0bf
Angerszhuuuu <angers.zhu@gmail.com>
2023-03-21 11:45:47 +0800

    Update CelebornRackResolverSuite.scala

--- common/src/test/scala/org/apache/celeborn/common/network/CelebornRackResolverSuite.scala
+++ common/src/test/scala/org/apache/celeborn/common/network/CelebornRackResolverSuite.scala
@@ -18,6 +18,7 @@
 package org.apache.celeborn.common.network
 
 import java.io.File
+
 import org.apache.hadoop.fs.CommonConfigurationKeysPublic.{NET_TOPOLOGY_NODE_SWITCH_MAPPING_IMPL_KEY, NET_TOPOLOGY_TABLE_MAPPING_FILE_KEY}
 import org.apache.hadoop.net.{Node, TableMapping}
 import org.apache.hadoop.shaded.com.google.common.base.Charsets
