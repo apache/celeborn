b3f786210ed493bcba181bd6161d154ef6a30150
Angerszhuuuu <angers.zhu@gmail.com>
2023-04-21 20:43:29 +0800

    Update ReducePartitionCommitHandler.scala

--- client/src/main/scala/org/apache/celeborn/client/commit/ReducePartitionCommitHandler.scala
+++ client/src/main/scala/org/apache/celeborn/client/commit/ReducePartitionCommitHandler.scala
@@ -98,8 +98,9 @@ class ReducePartitionCommitHandler(
     // Set empty HashSet during register shuffle, no NPE issue here.
     getReducerFileGroupRequest synchronized {
       stageEndShuffleSet.add(shuffleId)
-      getReducerFileGroupRequest.remove(shuffleId)
-    }.asScala.foreach(replyGetReducerFileGroup(_, shuffleId))
+    }
+    getReducerFileGroupRequest.remove(shuffleId)
+      .asScala.foreach(replyGetReducerFileGroup(_, shuffleId))
   }
 
   override def removeExpiredShuffle(shuffleId: Int): Unit = {
