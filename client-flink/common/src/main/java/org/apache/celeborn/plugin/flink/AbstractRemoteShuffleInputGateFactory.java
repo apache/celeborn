51bc15ccd6b1edb2d8140b881ae5f0ddda2b5a4e
Ethan Feng <ethanfeng@apache.org>
2023-05-04 10:25:52 +0800

    merge methods from utils

--- client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/AbstractRemoteShuffleInputGateFactory.java
+++ client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/AbstractRemoteShuffleInputGateFactory.java
@@ -20,6 +20,7 @@ package org.apache.celeborn.plugin.flink;
 
 import java.io.IOException;
 
+import org.apache.celeborn.plugin.flink.utils.FlinkUtils;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.deployment.InputGateDeploymentDescriptor;
 import org.apache.flink.runtime.io.network.buffer.BufferDecompressor;
@@ -69,12 +70,8 @@ public abstract class AbstractRemoteShuffleInputGateFactory {
       NetworkBufferPool networkBufferPool,
       int networkBufferSize) {
     this.celebornConf = conf;
-    long configuredMemorySize =
-        org.apache.celeborn.common.util.Utils.byteStringAsBytes(
-            PluginConf.getValue(flinkConf, PluginConf.MEMORY_PER_INPUT_GATE));
-    long minConfiguredMemorySize =
-        org.apache.celeborn.common.util.Utils.byteStringAsBytes(
-            PluginConf.getValue(flinkConf, PluginConf.MIN_MEMORY_PER_GATE));
+    long configuredMemorySize = FlinkUtils.byteStringValueAsBytes(flinkConf, PluginConf.MEMORY_PER_INPUT_GATE);
+    long minConfiguredMemorySize =FlinkUtils.byteStringValueAsBytes(flinkConf, PluginConf.MIN_MEMORY_PER_GATE);
     if (configuredMemorySize < minConfiguredMemorySize) {
       throw new IllegalArgumentException(
           String.format(
--- client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/AbstractRemoteShuffleResultPartitionFactory.java
+++ client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/AbstractRemoteShuffleResultPartitionFactory.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.celeborn.plugin.flink.utils.FlinkUtils;
 import org.apache.flink.annotation.VisibleForTesting;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.deployment.ResultPartitionDeploymentDescriptor;
@@ -72,12 +73,8 @@ public abstract class AbstractRemoteShuffleResultPartitionFactory {
       ResultPartitionManager partitionManager,
       BufferPoolFactory bufferPoolFactory,
       int networkBufferSize) {
-    long configuredMemorySize =
-        org.apache.celeborn.common.util.Utils.byteStringAsBytes(
-            PluginConf.getValue(flinkConf, PluginConf.MEMORY_PER_RESULT_PARTITION));
-    long minConfiguredMemorySize =
-        org.apache.celeborn.common.util.Utils.byteStringAsBytes(
-            PluginConf.getValue(flinkConf, PluginConf.MIN_MEMORY_PER_PARTITION));
+    long configuredMemorySize = FlinkUtils.byteStringValueAsBytes(flinkConf, PluginConf.MEMORY_PER_RESULT_PARTITION);
+    long minConfiguredMemorySize = FlinkUtils.byteStringValueAsBytes(flinkConf, PluginConf.MIN_MEMORY_PER_PARTITION);
     if (configuredMemorySize < minConfiguredMemorySize) {
       throw new IllegalArgumentException(
           String.format(
