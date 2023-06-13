66d3afebcd18e8b3659503d58acb37e8bcf13c30
Angerszhuuuu <angers.zhu@gmail.com>
2023-06-13 20:06:31 +0800

    [CELEBORN-494][PERF] RssInputStream fetch side support blacklist to avoid client side timeout in same worker multiple times during fetch
    ### What changes were proposed in this pull request?
    ####Test case
    ```
    executor instance 20
    
    SQL:
    SELECT count(1) from (select /*+ REPARTITION(100) */ * from spark_auxiliary.t50g) tmp;
    
    create connection timeout 10s
    
    Fetch chunk timeout 30s
    ```
    In the graph, the shuffle read time of `before` and `after` is always the same delay time.
    
    ##### Worker can't connect
    Before
    ![image](https://user-images.githubusercontent.com/46485123/229465520-9d751b40-2b8f-49d2-b350-a2278e3dd89e.png)
    
    After
    ![image](https://user-images.githubusercontent.com/46485123/229465552-88ac1ca4-24ad-4c30-9a46-0cdcae6bbfd5.png)
    
    ##### OpenStream stuck
    Before
    ![image](https://user-images.githubusercontent.com/46485123/229465629-68765a6a-2503-4018-8917-d49e47d5dccc.png)
    
    After
    ![image](https://user-images.githubusercontent.com/46485123/229465683-2f57b374-1c66-4819-93dd-cabee7ccb788.png)
    
    ##### Fetch chunk stuck
    Before
    ![image](https://user-images.githubusercontent.com/46485123/229465735-8d2f694b-1b4a-4984-b069-c4a308f41008.png)
    
    After
    ![image](https://user-images.githubusercontent.com/46485123/229465754-c2237d5a-6fb6-4d5b-819e-b7d86a1e88d7.png)
    
    ### Why are the changes needed?
    
    ### Does this PR introduce _any_ user-facing change?
    
    ### How was this patch tested?
    
    Closes #1406 from AngersZhuuuu/CELEBORN-494.
    
    Authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Signed-off-by: Shuang <lvshuang.tb@gmail.com>
    (cherry picked from commit 357add5b008d6f2d40b5fff8822e9abe1c082310)
    Signed-off-by: Shuang <lvshuang.tb@gmail.com>

--- common/src/main/java/org/apache/celeborn/common/protocol/PartitionLocation.java
+++ common/src/main/java/org/apache/celeborn/common/protocol/PartitionLocation.java
@@ -65,6 +65,8 @@ public class PartitionLocation implements Serializable {
   private RoaringBitmap mapIdBitMap;
   private transient String _hostPushPort;
 
+  private transient String _hostFetchPort;
+
   public PartitionLocation(PartitionLocation loc) {
     this.id = loc.id;
     this.epoch = loc.epoch;
@@ -78,6 +80,7 @@ public class PartitionLocation implements Serializable {
     this.storageInfo = loc.storageInfo;
     this.mapIdBitMap = loc.mapIdBitMap;
     this._hostPushPort = host + ":" + pushPort;
+    this._hostFetchPort = host + ":" + fetchPort;
   }
 
   public PartitionLocation(
@@ -151,6 +154,7 @@ public class PartitionLocation implements Serializable {
     this.storageInfo = hint;
     this.mapIdBitMap = mapIdBitMap;
     this._hostPushPort = host + ":" + pushPort;
+    this._hostFetchPort = host + ":" + fetchPort;
   }
 
   public int getId() {
@@ -204,6 +208,10 @@ public class PartitionLocation implements Serializable {
         + fetchPort
         + "-"
         + replicatePort;
+  }
+
+  public String hostAndFetchPort() {
+    return _hostFetchPort;
   }
 
   public String hostAndPushPort() {
