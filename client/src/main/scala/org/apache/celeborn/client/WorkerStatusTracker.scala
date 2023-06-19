c1d5af20cbf804efe03763551b03adc0b738f141
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2023-06-19 15:33:14 +0800

    refine code

--- client/src/main/scala/org/apache/celeborn/client/WorkerStatusTracker.scala
+++ client/src/main/scala/org/apache/celeborn/client/WorkerStatusTracker.scala
@@ -125,18 +125,14 @@ class WorkerStatusTracker(
       failedWorker.asScala.foreach {
         case (worker, (StatusCode.WORKER_SHUTDOWN, _)) =>
           shuttingWorkers.add(worker)
-        case (worker, (statusCode, registerTime)) =>
-          if (!blacklist.containsKey(worker)) {
-            blacklist.put(worker, (statusCode, registerTime))
-          } else {
-            statusCode match {
-              case StatusCode.NO_AVAILABLE_WORKING_DIR |
-                  StatusCode.RESERVE_SLOTS_FAILED |
-                  StatusCode.UNKNOWN_WORKER =>
-                blacklist.put(worker, (statusCode, blacklist.get(worker)._2))
-              case _ => // Not cover
-            }
-          }
+        case (worker, (statusCode, registerTime)) if !blacklist.containsKey(worker) =>
+          blacklist.put(worker, (statusCode, registerTime))
+        case (worker, (statusCode, _))
+            if statusCode == StatusCode.NO_AVAILABLE_WORKING_DIR ||
+              statusCode == StatusCode.RESERVE_SLOTS_FAILED ||
+              statusCode == StatusCode.UNKNOWN_WORKER =>
+          blacklist.put(worker, (statusCode, blacklist.get(worker)._2))
+        case _ => // Not cover
       }
     }
   }
