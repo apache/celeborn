1b3ec61690e3ff23565c953f51cc815342d52c84
Fu Chen <cfmcgrady@gmail.com>
2023-06-26 13:45:27 +0800

    [CELEBORN-711][TEST] Rework PushDataTimeoutTest
    ### What changes were proposed in this pull request?
    
    1. separated push data timeout tests and push merge data timeout tests in `PushDataTimeoutTest`
    2. updated the test results assertion
    3. rework `pushdata timeout will add to blacklist`
    
    ### Why are the changes needed?
    
    ensure that the timeout behavior is correctly implemented
    
    https://github.com/apache/incubator-celeborn/pull/1613#discussion_r1236423721
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    existing tests
    
    Closes #1620 from cfmcgrady/push-timeout-test.
    
    Authored-by: Fu Chen <cfmcgrady@gmail.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
+++ client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/RssShuffleManager.java
@@ -310,4 +310,9 @@ public class RssShuffleManager implements ShuffleManager {
     pusherIdx.incrementAndGet();
     return pusherThread;
   }
+
+  // for testing
+  public LifecycleManager getLifecycleManager() {
+    return this.lifecycleManager;
+  }
 }
