288d3c58346e9c307c7cce34a29478277cbf9701
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2023-07-11 16:04:23 +0800

    minor

--- worker/src/test/java/org/apache/celeborn/service/deploy/worker/storage/FileWriterSuiteJ.java
+++ worker/src/test/java/org/apache/celeborn/service/deploy/worker/storage/FileWriterSuiteJ.java
@@ -611,7 +611,7 @@ public class FileWriterSuiteJ {
     assertEquals(fileInfo.getLastChunkOffset(), 2048);
     assertEquals(fileInfo.getChunkOffsets().get(1) - fileInfo.getChunkOffsets().get(0), 1024);
 
-    // case 5.1: write 2048B with trim; Without [CELEBORN-626][FOLLOWUP] this case will fail
+    // case 5.1: write 2048B with trim; without PR #1702 this case will fail
     file = getTemporaryFile();
     fileInfo = new FileInfo(file, userIdentifier);
     fileWriter =
@@ -679,8 +679,7 @@ public class FileWriterSuiteJ {
     assertEquals(fileInfo.getLastChunkOffset(), 4096);
     assertEquals(fileInfo.getChunkOffsets().get(3) - fileInfo.getChunkOffsets().get(2), 2048);
 
-    // case 7.2: write 4097B with 3 chunks with trim; Without [CELEBORN-626][FOLLOWUP] this case
-    // will fail
+    // case 7.2: write 4097B with 3 chunks with trim; without PR #1702 this case will fail
     file = getTemporaryFile();
     fileInfo = new FileInfo(file, userIdentifier);
     fileWriter =
