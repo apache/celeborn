1642090f9f06e566e51d39c91ae6ce0d443b5fff
Angerszhuuuu <angers.zhu@gmail.com>
2023-07-12 14:16:19 +0800

    [CELEBORN-781] Refactor RPC message type name
    ### What changes were proposed in this pull request?
    After https://github.com/apache/incubator-celeborn/pull/1658 merged, we can format the message type now.
    
    ### Why are the changes needed?
    
    ### Does this PR introduce _any_ user-facing change?
    
    ### How was this patch tested?
    
    Closes #1696 from AngersZhuuuu/CELEBORN-731.
    
    Authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Signed-off-by: Angerszhuuuu <angers.zhu@gmail.com>

--- common/src/main/scala/org/apache/celeborn/common/protocol/message/ControlMessages.scala
+++ common/src/main/scala/org/apache/celeborn/common/protocol/message/ControlMessages.scala
@@ -416,10 +416,10 @@ object ControlMessages extends Logging {
   // TODO change message type to GeneratedMessageV3
   def toTransportMessage(message: Any): TransportMessage = message match {
     case _: PbCheckForWorkerTimeoutOrBuilder =>
-      new TransportMessage(MessageType.CHECK_FOR_WORKER_TIMEOUT, null)
+      new TransportMessage(MessageType.CHECK_WORKER_TIMEOUT, null)
 
     case CheckForApplicationTimeOut =>
-      new TransportMessage(MessageType.CHECK_FOR_APPLICATION_TIMEOUT, null)
+      new TransportMessage(MessageType.CHECK_APPLICATION_TIMEOUT, null)
 
     case CheckForHDFSExpiredDirsTimeout =>
       new TransportMessage(MessageType.CHECK_FOR_HDFS_EXPIRED_DIRS_TIMEOUT, null)
@@ -463,7 +463,7 @@ object ControlMessages extends Logging {
         .addAllExpiredShuffleKeys(expiredShuffleKeys)
         .setRegistered(registered)
         .build().toByteArray
-      new TransportMessage(MessageType.HEARTBEAT_RESPONSE, payload)
+      new TransportMessage(MessageType.HEARTBEAT_FROM_WORKER_RESPONSE, payload)
 
     case pb: PbRegisterShuffle =>
       new TransportMessage(MessageType.REGISTER_SHUFFLE, pb.toByteArray)
@@ -523,7 +523,7 @@ object ControlMessages extends Logging {
       new TransportMessage(MessageType.REQUEST_SLOTS_RESPONSE, payload)
 
     case pb: PbRevive =>
-      new TransportMessage(MessageType.REVIVE, pb.toByteArray)
+      new TransportMessage(MessageType.CHANGE_LOCATION, pb.toByteArray)
 
     case pb: PbChangeLocationResponse =>
       new TransportMessage(MessageType.CHANGE_LOCATION_RESPONSE, pb.toByteArray)
@@ -799,7 +799,7 @@ object ControlMessages extends Logging {
           estimatedAppDiskUsage,
           pbHeartbeatFromWorker.getRequestId)
 
-      case HEARTBEAT_RESPONSE_VALUE =>
+      case HEARTBEAT_FROM_WORKER_RESPONSE_VALUE =>
         val pbHeartbeatFromWorkerResponse =
           PbHeartbeatFromWorkerResponse.parseFrom(message.getPayload)
         val expiredShuffleKeys = new util.HashSet[String]()
@@ -852,7 +852,7 @@ object ControlMessages extends Logging {
           PbSerDeUtils.fromPbWorkerResource(
             pbRequestSlotsResponse.getWorkerResourceMap))
 
-      case REVIVE_VALUE =>
+      case CHANGE_LOCATION_VALUE =>
         PbRevive.parseFrom(message.getPayload)
 
       case CHANGE_LOCATION_RESPONSE_VALUE =>
@@ -1031,10 +1031,10 @@ object ControlMessages extends Logging {
       case ONE_WAY_MESSAGE_RESPONSE_VALUE =>
         OneWayMessageResponse
 
-      case CHECK_FOR_WORKER_TIMEOUT_VALUE =>
+      case CHECK_WORKER_TIMEOUT_VALUE =>
         pbCheckForWorkerTimeout
 
-      case CHECK_FOR_APPLICATION_TIMEOUT_VALUE =>
+      case CHECK_APPLICATION_TIMEOUT_VALUE =>
         CheckForApplicationTimeOut
 
       case CHECK_FOR_HDFS_EXPIRED_DIRS_TIMEOUT_VALUE =>
