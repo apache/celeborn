a167b63d9bb91197ff7358770ae8b03b7171a406
Xiduo You <ulyssesyou18@gmail.com>
2023-08-25 17:56:36 +0800

    fix style

--- client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SparkShuffleManager.java
+++ client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SparkShuffleManager.java
@@ -130,9 +130,11 @@ public class SparkShuffleManager implements ShuffleManager {
     if (fallbackPolicyRunner.applyAllFallbackPolicy(
         lifecycleManager, dependency.partitioner().numPartitions())) {
       if (conf.getBoolean("spark.dynamicAllocation.enabled", false)) {
-        logger.error("DRA is enabled but we fallback to vanilla Spark SortShuffleManager for " +
-            "shuffle: {} due to fallback policy. It may cause block can not found when reducer " +
-                "task fetch data.", shuffleId);
+        logger.error(
+            "DRA is enabled but we fallback to vanilla Spark SortShuffleManager for "
+                + "shuffle: {} due to fallback policy. It may cause block can not found when reducer "
+                + "task fetch data.",
+            shuffleId);
       } else {
         logger.warn("Fallback to vanilla Spark SortShuffleManager for shuffle: {}", shuffleId);
       }
