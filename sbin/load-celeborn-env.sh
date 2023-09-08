ada12a2c0e9762d546a891648ab42a8fca6c653c
Jun He <xuehaijuxian@gmail.com>
2023-09-08 19:33:24 +0800

    [CELEBORN-900] Prefer to use jemalloc for memory allocation
    ### What changes were proposed in this pull request?
    
    Only the Dockfile needs to change in this pr.
    
    ### Why are the changes needed?
    
    When deploying celeborn for flink on kubernetes, Introducing jemalloc can improve pod memory usage.
    
    ### Does this PR introduce _any_ user-facing change?
    None
    
    ### How was this patch tested?
    Maybe starting a production job to test the memory usage improvement is needed.
    
    Closes #1824 from mddxhj/feature/introduce_jemalloc.
    
    Authored-by: Jun He <xuehaijuxian@gmail.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- sbin/load-celeborn-env.sh
+++ sbin/load-celeborn-env.sh
@@ -69,3 +69,24 @@ if [ "$CELEBORN_PID_DIR" = "" ]; then
   export CELEBORN_PID_DIR="${CELEBORN_HOME}/pids"
 fi
 
+# jemalloc memory allocator is enabled by default, you may set DISABLE_JEMALLOC to true in celeborn-env.sh if it's not as you wish.
+maybe_enable_jemalloc() {
+  if [ "${DISABLE_JEMALLOC:-false}" == "false" ]; then
+    JEMALLOC_PATH="/usr/lib/$(uname -m)-linux-gnu/libjemalloc.so"
+    JEMALLOC_FALLBACK="/usr/lib/x86_64-linux-gnu/libjemalloc.so"
+    if [ -f "$JEMALLOC_PATH" ]; then
+      export LD_PRELOAD="$LD_PRELOAD:$JEMALLOC_PATH"
+    elif [ -f "$JEMALLOC_FALLBACK" ]; then
+      export LD_PRELOAD="$LD_PRELOAD:$JEMALLOC_FALLBACK"
+    else
+      if [ "$JEMALLOC_PATH" == "$JEMALLOC_FALLBACK" ]; then
+        MSG_PATH="$JEMALLOC_PATH"
+      else
+        MSG_PATH="$JEMALLOC_PATH and $JEMALLOC_FALLBACK"
+      fi
+      echo "WARNING: attempted to load jemalloc from $MSG_PATH but the library couldn't be found. glibc will be used instead."
+    fi
+  fi
+}
+maybe_enable_jemalloc
+
