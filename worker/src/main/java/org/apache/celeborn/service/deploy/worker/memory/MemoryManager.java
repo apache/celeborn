7286c4288b569487d0ec25fd20a3670d644da0fe
zwangsheng <binjieyang@apache.org>
2023-09-12 17:34:45 +0800

    fix ci

--- worker/src/main/java/org/apache/celeborn/service/deploy/worker/memory/MemoryManager.java
+++ worker/src/main/java/org/apache/celeborn/service/deploy/worker/memory/MemoryManager.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 
