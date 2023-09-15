18eda34d77595dcb4a96b9ee23f78d74e0da940d
sychen <sychen@ctrip.com>
2023-09-15 11:36:14 +0800

    grep Dependencies classpath

--- dev/dependencies.sh
+++ dev/dependencies.sh
@@ -39,8 +39,9 @@ DEP=""
 function mvn_build_classpath() {
   $MVN -P$MODULE clean install -DskipTests -am -pl $MVN_MODULES
   $MVN -P$MODULE dependency:build-classpath -am -pl $MVN_MODULES | \
-    grep -v "INFO\|WARN" | \
-    grep -v "Downloading from" | \
+    grep -A1 "Dependencies classpath:" | \
+    grep -v "^--$" | \
+    grep -v "Dependencies classpath:" | \
     # This will skip the first two lines
     tail -n +3 | \
     tr ":" "\n" | \
