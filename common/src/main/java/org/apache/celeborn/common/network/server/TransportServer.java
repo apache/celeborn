f4ffcbeeee816a695850089a11fcaffd75efe095
sychen <sychen@ctrip.com>
2023-09-28 19:07:12 +0800

    [CELEBORN-1014] Output log with bound address and port
    ### What changes were proposed in this pull request?
    
    ### Why are the changes needed?
    Make it easy for administrators to find the address of the http service bindings.
    
    ### Does this PR introduce _any_ user-facing change?
    
    ### How was this patch tested?
    
    ```
    23/09/28 17:10:50,465 INFO [main] HttpServer: master: HttpServer started on port 9983.
    ```
    
    PR
    ```
    23/09/28 17:28:29,797 INFO [main] HttpServer: master: HttpServer started on clb-3 with port 9983.
    ```
    
    Closes #1947 from cxzl25/CELEBORN-1014.
    
    Lead-authored-by: sychen <sychen@ctrip.com>
    Co-authored-by: Cheng Pan <pan3793@gmail.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>
    (cherry picked from commit 7e944c1a50400931f67f4b42e44239591fe64700)
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- common/src/main/java/org/apache/celeborn/common/network/server/TransportServer.java
+++ common/src/main/java/org/apache/celeborn/common/network/server/TransportServer.java
@@ -116,7 +116,7 @@ public class TransportServer implements Closeable {
     channelFuture.syncUninterruptibly();
 
     port = ((InetSocketAddress) channelFuture.channel().localAddress()).getPort();
-    logger.debug("Shuffle server started on port: {}", port);
+    logger.debug("Shuffle server started on {}:{}", address.getHostString(), port);
   }
 
   protected void initializeChannel(ServerBootstrap bootstrap) {
