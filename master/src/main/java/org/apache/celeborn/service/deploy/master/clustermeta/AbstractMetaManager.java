277ba9e9cf6a59903d909d40f9d0504af6b57b4c
SteNicholas <programgeek@163.com>
2023-11-06 16:00:38 +0800

    [CELEBORN-448] Support exclude worker manually

--- master/src/main/java/org/apache/celeborn/service/deploy/master/clustermeta/AbstractMetaManager.java
+++ master/src/main/java/org/apache/celeborn/service/deploy/master/clustermeta/AbstractMetaManager.java
@@ -187,9 +187,7 @@ public abstract class AbstractMetaManager implements IMetadataHandler {
         && (((disks.isEmpty() || healthyDiskNum <= 0) && !conf.hasHDFSStorage()) || highWorkload)) {
       LOG.debug("Worker: {} num total slots is 0, add to excluded list", worker);
       excludedWorkers.add(worker);
-    } else if ((availableSlots.get() > 0 || conf.hasHDFSStorage())
-        && !highWorkload
-        && !manuallyExcludedWorkers.contains(worker)) {
+    } else if ((availableSlots.get() > 0 || conf.hasHDFSStorage()) && !highWorkload) {
       // only unblack if numSlots larger than 0
       excludedWorkers.remove(worker);
     }
