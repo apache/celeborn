4db8623d9c1a2a965b35845f0317511fc311031d
Fu Chen <cfmcgrady@gmail.com>
2023-11-06 13:52:12 +0800

    Update project/CelebornBuild.scala

--- project/CelebornBuild.scala
+++ project/CelebornBuild.scala
@@ -690,7 +690,7 @@ trait SparkClientProjects {
   
         (assembly / assemblyMergeStrategy) := {
           case m if m.toLowerCase(Locale.ROOT).endsWith("manifest.mf") => MergeStrategy.discard
-          case m if m == "META-INF/LICENSE" || m == "META-INF/NOTICE" => MergeStrategy.preferProject
+          case "META-INF/LICENSE" | "META-INF/NOTICE" => MergeStrategy.preferProject
           case PathList(ps@_*) if Assembly.isLicenseFile(ps.last) => MergeStrategy.discard
           // Drop all proto files that are not needed as artifacts of the build.
           case m if m.toLowerCase(Locale.ROOT).endsWith(".proto") => MergeStrategy.discard
