18767f2a8bca06683a1e227b2431d6894662604f
Chandni Singh <singh.chandni@gmail.com>
2023-11-28 17:06:04 +0530

    Address a comment from the draft PR

--- common/src/main/java/org/apache/celeborn/common/network/client/TransportClientFactory.java
+++ common/src/main/java/org/apache/celeborn/common/network/client/TransportClientFactory.java
@@ -278,8 +278,8 @@ public class TransportClientFactory implements Closeable {
     logger.info(
         "Successfully created connection to {} after {} ms ({} ms spent in bootstraps)",
         address,
-        (postBootstrap - preConnect) / 1000000,
-        (postBootstrap - preBootstrap) / 1000000);
+        Duration.ofNanos(postBootstrap - preConnect).toMillis(),
+        Duration.ofNanos(postBootstrap - preBootstrap).toMillis());
 
     logger.debug(
         "Connection from {} to {} successful", client.getChannel().localAddress(), address);
