7ba5168059da8bbdcc862f80bf130907eef03f39
Cheng Pan <chengpan@apache.org>
2024-01-05 20:55:47 +0800

    fi

--- common/src/main/java/org/apache/celeborn/common/util/JavaUtils.java
+++ common/src/main/java/org/apache/celeborn/common/util/JavaUtils.java
@@ -46,7 +46,7 @@ import org.apache.celeborn.common.network.util.ByteUnit;
  * General utilities available in the network package. Many of these are sourced from Spark's own
  * Utils, just accessible within this package.
  *
- * Some methods of this class are copied from Apache Spark
+ * <p>Some methods of this class are copied from Apache Spark
  */
 public class JavaUtils {
   private static final Logger logger = LoggerFactory.getLogger(JavaUtils.class);
