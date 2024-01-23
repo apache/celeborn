43ffe5d45d0538a16fd52373f2a7082d2de49dec
CodingCat <zhunansjtu@gmail.com>
2024-01-23 14:53:57 -0800

    stylistic fix

--- worker/src/test/scala/org/apache/celeborn/service/deploy/MiniClusterFeature.scala
+++ worker/src/test/scala/org/apache/celeborn/service/deploy/MiniClusterFeature.scala
@@ -24,6 +24,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import scala.collection.JavaConverters._
 import scala.util.Random
+
 import org.apache.celeborn.common.CelebornConf
 import org.apache.celeborn.common.internal.Logging
 import org.apache.celeborn.common.util.{CelebornExitKind, Utils}
