ec1ba214c0a46b8f62f49e9c40e301315fdb9e50
Angerszhuuuu <angers.zhu@gmail.com>
2024-01-23 14:56:58 +0800

    Update LocalPartitionReader.java

--- client/src/main/java/org/apache/celeborn/client/read/LocalPartitionReader.java
+++ client/src/main/java/org/apache/celeborn/client/read/LocalPartitionReader.java
@@ -83,7 +83,7 @@ public class LocalPartitionReader implements PartitionReader {
         if (readLocalShufflePool == null) {
           readLocalShufflePool =
               ThreadUtils.newDaemonCachedThreadPool(
-                  "celeborn-client-local-shuffle-reader-thread",
+                  "celeborn-client-local-shuffle-reader",
                   conf.readLocalShuffleThreads(),
                   60);
         }
