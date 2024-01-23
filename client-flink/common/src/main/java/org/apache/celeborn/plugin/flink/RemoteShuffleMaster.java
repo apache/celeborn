882dd0ebebe4e08adf85637d9ea6cc085b25e457
Angerszhuuuu <angers.zhu@gmail.com>
2024-01-23 14:12:01 +0800

    Update RemoteShuffleMaster.java

--- client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/RemoteShuffleMaster.java
+++ client-flink/common/src/main/java/org/apache/celeborn/plugin/flink/RemoteShuffleMaster.java
@@ -57,7 +57,8 @@ public class RemoteShuffleMaster implements ShuffleMaster<RemoteShuffleDescripto
   private final ShuffleTaskInfo shuffleTaskInfo = new ShuffleTaskInfo();
   private ShuffleResourceTracker shuffleResourceTracker;
   private final ScheduledExecutorService executor =
-      ThreadUtils.newDaemonSingleThreadScheduledExecutor("celeborn-client-remote-shuffle-master-executor");
+      ThreadUtils.newDaemonSingleThreadScheduledExecutor(
+          "celeborn-client-remote-shuffle-master-executor");
   private final ResultPartitionAdapter resultPartitionDelegation;
   private final long lifecycleManagerTimestamp;
 
