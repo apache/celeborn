074aeda233df234ca6d7a3c2fa69e49c494bae3b
Angerszhuuuu <angers.zhu@gmail.com>
2024-01-25 20:54:03 +0800

    Update StorageManagerSuite.scala

--- worker/src/test/scala/org/apache/celeborn/service/deploy/worker/storage/StorageManagerSuite.scala
+++ worker/src/test/scala/org/apache/celeborn/service/deploy/worker/storage/StorageManagerSuite.scala
@@ -20,8 +20,6 @@ package org.apache.celeborn.service.deploy.worker.storage
 import org.apache.celeborn.CelebornFunSuite
 import org.apache.celeborn.common.CelebornConf
 import org.apache.celeborn.common.CelebornConf.{WORKER_GRACEFUL_SHUTDOWN_ENABLED, WORKER_GRACEFUL_SHUTDOWN_RECOVER_PATH}
-import org.apache.celeborn.common.metrics.MetricsSystem
-import org.apache.celeborn.common.metrics.source.ThreadPoolSource
 import org.apache.celeborn.service.deploy.worker.WorkerSource
 
 class StorageManagerSuite extends CelebornFunSuite {
@@ -29,10 +27,7 @@ class StorageManagerSuite extends CelebornFunSuite {
   test("[CELEBORN-926] saveAllCommittedFileInfosToDB cause IllegalMonitorStateException") {
     val conf = new CelebornConf().set(WORKER_GRACEFUL_SHUTDOWN_ENABLED, true)
       .set(WORKER_GRACEFUL_SHUTDOWN_RECOVER_PATH, "/tmp/recover")
-    val storageManager = new StorageManager(
-      conf,
-      new WorkerSource(conf),
-      new ThreadPoolSource(conf, MetricsSystem.ROLE_WORKER))
+    val storageManager = new StorageManager(conf, new WorkerSource(conf))
     // should not throw IllegalMonitorStateException exception
     storageManager.saveAllCommittedFileInfosToDB()
   }
