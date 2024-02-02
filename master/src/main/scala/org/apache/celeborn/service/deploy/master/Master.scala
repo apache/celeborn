f7c8c9dd751208c579e76332ed33e03f06d6265b
SteNicholas <programgeek@163.com>
2024-02-02 20:21:43 +0800

    [CELEBORN-1174][0.4] Introduce application dimension resource consumption metrics
    ### What changes were proposed in this pull request?
    
    Cherry pick #2161.
    
    Introduce application dimension resource consumption metrics for `ResourceConsumptionSource`.
    
    ### Why are the changes needed?
    
    `ResourceConsumption` namespace metrics are generated for each user and they are identified using a metric tag at present. It's recommended to introduce application dimension resource consumption metrics that expose application dimension resource consumption of Master and Worker. By monitoring resource consumption in the application dimension, you can obtain the actual situation of application resource consumption.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    - `WorkerInfoSuite#WorkerInfo toString output`
    - `PbSerDeUtilsTest#fromAndToPbResourceConsumption`
    - `MasterStateMachineSuitej#testObjSerde`
    
    Closes #2279 from SteNicholas/CELEBORN-1174-0.4.
    
    Authored-by: SteNicholas <programgeek@163.com>
    Signed-off-by: SteNicholas <programgeek@163.com>

--- master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
@@ -42,8 +42,7 @@ import org.apache.celeborn.common.protocol.message.{ControlMessages, StatusCode}
 import org.apache.celeborn.common.protocol.message.ControlMessages._
 import org.apache.celeborn.common.quota.{QuotaManager, ResourceConsumption}
 import org.apache.celeborn.common.rpc._
-import org.apache.celeborn.common.rpc.netty.NettyRpcEnv
-import org.apache.celeborn.common.util.{CelebornHadoopUtils, JavaUtils, PbSerDeUtils, ThreadUtils, Utils}
+import org.apache.celeborn.common.util.{CelebornHadoopUtils, CollectionUtils, JavaUtils, PbSerDeUtils, ThreadUtils, Utils}
 import org.apache.celeborn.server.common.{HttpService, Service}
 import org.apache.celeborn.service.deploy.master.clustermeta.SingleMasterMetaManager
 import org.apache.celeborn.service.deploy.master.clustermeta.ha.{HAHelper, HAMasterMetaManager, MetaHandler}
@@ -783,6 +782,8 @@ private[celeborn] class Master(
     nonEagerHandler.submit(new Runnable {
       override def run(): Unit = {
         statusSystem.handleAppLost(appId, requestId)
+        // Resource consumption source should remove lose application gauges.
+        removeAppResourceConsumption(appId)
         logInfo(s"Removed application $appId")
         if (hasHDFSStorage) {
           checkAndCleanExpiredAppDirsOnHDFS(appId)
@@ -790,6 +791,30 @@ private[celeborn] class Master(
         context.reply(ApplicationLostResponse(StatusCode.SUCCESS))
       }
     })
+  }
+
+  private def removeAppResourceConsumption(appId: String): Unit = {
+    removeResourceConsumptionGauge(
+      ResourceConsumptionSource.DISK_FILE_COUNT,
+      appId)
+    removeResourceConsumptionGauge(
+      ResourceConsumptionSource.DISK_BYTES_WRITTEN,
+      appId)
+    removeResourceConsumptionGauge(
+      ResourceConsumptionSource.HDFS_FILE_COUNT,
+      appId)
+    removeResourceConsumptionGauge(
+      ResourceConsumptionSource.HDFS_BYTES_WRITTEN,
+      appId)
+  }
+
+  private def removeResourceConsumptionGauge(
+      resourceConsumptionName: String,
+      appId: String): Unit = {
+    resourceConsumptionSource.removeGauge(
+      resourceConsumptionName,
+      ResourceConsumptionSource.APPLICATION_LABEL,
+      appId)
   }
 
   private def checkAndCleanExpiredAppDirsOnHDFS(expiredDir: String = ""): Unit = {
@@ -853,55 +878,77 @@ private[celeborn] class Master(
     statusSystem.handleRemoveWorkersUnavailableInfo(unavailableWorkers, requestId)
   }
 
-  private def computeUserResourceConsumption(userIdentifier: UserIdentifier)
-      : ResourceConsumption = {
-    val current = System.currentTimeMillis()
-    if (userResourceConsumptions.containsKey(userIdentifier)) {
-      val resourceConsumptionAndUpdateTime = userResourceConsumptions.get(userIdentifier)
-      if (current - resourceConsumptionAndUpdateTime._2 > masterResourceConsumptionInterval) {
-        val newResourceConsumption = statusSystem.workers.asScala.flatMap { workerInfo =>
-          workerInfo.userResourceConsumption.asScala.get(userIdentifier)
-        }.foldRight(ResourceConsumption(0, 0, 0, 0))(_ add _)
-        userResourceConsumptions.put(userIdentifier, (newResourceConsumption, current))
-        newResourceConsumption
-      } else {
-        resourceConsumptionAndUpdateTime._1
+  private def handleResourceConsumption(userIdentifier: UserIdentifier): ResourceConsumption = {
+    val userResourceConsumption = computeUserResourceConsumption(userIdentifier)
+    gaugeResourceConsumption(userIdentifier)
+    val subResourceConsumptions = userResourceConsumption.subResourceConsumptions
+    if (CollectionUtils.isNotEmpty(subResourceConsumptions)) {
+      subResourceConsumptions.asScala.keys.foreach { gaugeResourceConsumption(userIdentifier, _) }
+    }
+    userResourceConsumption
+  }
+
+  private def gaugeResourceConsumption(
+      userIdentifier: UserIdentifier,
+      applicationId: String = null): Unit = {
+    val resourceConsumptionLabel =
+      if (applicationId == null) userIdentifier.toMap
+      else userIdentifier.toMap + (ResourceConsumptionSource.APPLICATION_LABEL -> applicationId)
+    resourceConsumptionSource.addGauge(
+      ResourceConsumptionSource.DISK_FILE_COUNT,
+      resourceConsumptionLabel) { () =>
+      computeResourceConsumption(userIdentifier).diskFileCount
+    }
+    resourceConsumptionSource.addGauge(
+      ResourceConsumptionSource.DISK_BYTES_WRITTEN,
+      resourceConsumptionLabel) { () =>
+      computeResourceConsumption(userIdentifier).diskBytesWritten
+    }
+    resourceConsumptionSource.addGauge(
+      ResourceConsumptionSource.HDFS_FILE_COUNT,
+      resourceConsumptionLabel) { () =>
+      computeResourceConsumption(userIdentifier).hdfsFileCount
+    }
+    resourceConsumptionSource.addGauge(
+      ResourceConsumptionSource.HDFS_BYTES_WRITTEN,
+      resourceConsumptionLabel) { () =>
+      computeResourceConsumption(userIdentifier).hdfsBytesWritten
+    }
+  }
+
+  private def computeResourceConsumption(
+      userIdentifier: UserIdentifier,
+      applicationId: String = null): ResourceConsumption = {
+    val newResourceConsumption = computeUserResourceConsumption(userIdentifier)
+    if (applicationId == null) {
+      val current = System.currentTimeMillis()
+      if (userResourceConsumptions.containsKey(userIdentifier)) {
+        val resourceConsumptionAndUpdateTime = userResourceConsumptions.get(userIdentifier)
+        if (current - resourceConsumptionAndUpdateTime._2 <= masterResourceConsumptionInterval) {
+          return resourceConsumptionAndUpdateTime._1
+        }
       }
-    } else {
-      val newResourceConsumption = statusSystem.workers.asScala.flatMap { workerInfo =>
-        workerInfo.userResourceConsumption.asScala.get(userIdentifier)
-      }.foldRight(ResourceConsumption(0, 0, 0, 0))(_ add _)
       userResourceConsumptions.put(userIdentifier, (newResourceConsumption, current))
       newResourceConsumption
+    } else {
+      newResourceConsumption.subResourceConsumptions.get(applicationId)
     }
+  }
+
+  private def computeUserResourceConsumption(
+      userIdentifier: UserIdentifier): ResourceConsumption = {
+    val (resourceConsumption, subResourceConsumptions) = statusSystem.workers.asScala.flatMap {
+      workerInfo => workerInfo.userResourceConsumption.asScala.get(userIdentifier)
+    }.foldRight((ResourceConsumption(0, 0, 0, 0), Map.empty[String, ResourceConsumption]))(
+      _ addWithSubResourceConsumptions _)
+    resourceConsumption.subResourceConsumptions = subResourceConsumptions.asJava
+    resourceConsumption
   }
 
   private def handleCheckQuota(
       userIdentifier: UserIdentifier,
       context: RpcCallContext): Unit = {
-
-    resourceConsumptionSource.addGauge(
-      ResourceConsumptionSource.DISK_FILE_COUNT,
-      userIdentifier.toMap) { () =>
-      computeUserResourceConsumption(userIdentifier).diskFileCount
-    }
-    resourceConsumptionSource.addGauge(
-      ResourceConsumptionSource.DISK_BYTES_WRITTEN,
-      userIdentifier.toMap) { () =>
-      computeUserResourceConsumption(userIdentifier).diskBytesWritten
-    }
-    resourceConsumptionSource.addGauge(
-      ResourceConsumptionSource.HDFS_FILE_COUNT,
-      userIdentifier.toMap) { () =>
-      computeUserResourceConsumption(userIdentifier).hdfsFileCount
-    }
-    resourceConsumptionSource.addGauge(
-      ResourceConsumptionSource.HDFS_BYTES_WRITTEN,
-      userIdentifier.toMap) { () =>
-      computeUserResourceConsumption(userIdentifier).hdfsBytesWritten
-    }
-
-    val userResourceConsumption = computeUserResourceConsumption(userIdentifier)
+    val userResourceConsumption = handleResourceConsumption(userIdentifier)
     val quota = quotaManager.getQuota(userIdentifier)
     val (isAvailable, reason) =
       quota.checkQuotaSpaceAvailable(userIdentifier, userResourceConsumption)
