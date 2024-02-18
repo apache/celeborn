a6a01b36d409758523bcf843228e40d832a72503
Angerszhuuuu <angers.zhu@gmail.com>
2024-02-18 15:01:17 +0800

    Update FsConfigServiceImpl.java

--- service/src/main/java/org/apache/celeborn/server/common/service/config/FsConfigServiceImpl.java
+++ service/src/main/java/org/apache/celeborn/server/common/service/config/FsConfigServiceImpl.java
@@ -65,19 +65,19 @@ public class FsConfigServiceImpl extends BaseConfigServiceImpl implements Config
             ((Map<String, Object>) settings.get(CONF_CONFIG))
                 .entrySet().stream()
                     .collect(Collectors.toMap(Map.Entry::getKey, a -> a.getValue().toString()));
-
         if (ConfigLevel.TENANT.name().equals(level)) {
           TenantConfig tenantConfig = new TenantConfig(this, tenantId, "", config);
           tenantConfs.put(tenantId, tenantConfig);
-          List<Map<String, Object>> users =
-              (List<Map<String, Object>>) settings.get(CONF_TENANT_USERS);
-          if(users != null) {
+          if (settings.containsKey(CONF_TENANT_USERS)) {
+            List<Map<String, Object>> users =
+                (List<Map<String, Object>>) settings.get(CONF_TENANT_USERS);
             for (Map<String, Object> userSetting : users) {
               String name = (String) userSetting.get(CONF_TENANT_NAME);
               Map<String, String> userConfig =
                   ((Map<String, Object>) userSetting.get(CONF_CONFIG))
                       .entrySet().stream()
-                      .collect(Collectors.toMap(Map.Entry::getKey, a -> a.getValue().toString()));
+                          .collect(
+                              Collectors.toMap(Map.Entry::getKey, a -> a.getValue().toString()));
               TenantConfig tenantUserConfig = new TenantConfig(this, tenantId, name, userConfig);
               tenantUserConfs.put(Pair.of(tenantId, name), tenantUserConfig);
             }
