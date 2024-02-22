63f0232fd6f5e8ff85569cedfcf4cf1ba49653a6
Angerszhuuuu <angers.zhu@gmail.com>
2024-02-22 11:27:15 +0800

    Update DynamicConfig.java

--- service/src/main/java/org/apache/celeborn/server/common/service/config/DynamicConfig.java
+++ service/src/main/java/org/apache/celeborn/server/common/service/config/DynamicConfig.java
@@ -40,7 +40,7 @@ import org.apache.celeborn.common.util.Utils;
 public abstract class DynamicConfig {
   private static final Logger LOG = LoggerFactory.getLogger(DynamicConfig.class);
   protected Map<String, String> configs = new HashMap<>();
-  protected Quota quota = CelebornConf.DEFAULT_QUOTA();
+  protected Quota quota = currentQuota();
 
   public abstract DynamicConfig getParentLevelConfig();
 
