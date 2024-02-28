6597972e5f06dd5d9d07ad14d67e4a673e247968
Angerszhuuuu <angers.zhu@gmail.com>
2024-02-28 11:58:32 +0800

    Update Worker.scala

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Worker.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Worker.scala
@@ -535,10 +535,6 @@ private[celeborn] class Worker(
     val resourceConsumptionSnapshot = storageManager.userResourceConsumptionSnapshot()
     resourceConsumptionSnapshot.foreach { case (userIdentifier, userResourceConsumption) =>
       gaugeResourceConsumption(userIdentifier)
-      val subResourceConsumptions = userResourceConsumption.subResourceConsumptions
-      if (CollectionUtils.isNotEmpty(subResourceConsumptions)) {
-        subResourceConsumptions.asScala.keys.foreach { gaugeResourceConsumption(userIdentifier, _) }
-      }
     }
     workerInfo.updateThenGetUserResourceConsumption(resourceConsumptionSnapshot.asJava)
   }
