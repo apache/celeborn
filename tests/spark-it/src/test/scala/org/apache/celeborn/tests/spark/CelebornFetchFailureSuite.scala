2ed0c28ff7f6a988906264ee5fe9bc11362f1ebb
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2024-03-03 15:33:35 +0800

    [CELEBORN-1300] Optimize CelebornInputStreamImpl's memory usage

--- tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFetchFailureSuite.scala
+++ tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFetchFailureSuite.scala
@@ -96,193 +96,205 @@ class CelebornFetchFailureSuite extends AnyFunSuite
   }
 
   test("celeborn spark integration test - Fetch Failure") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .config(
-        "spark.shuffle.manager",
-        "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
+        .config(
+          "spark.shuffle.manager",
+          "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
+        .getOrCreate()
 
-    val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
-    val hook = new ShuffleReaderGetHook(celebornConf)
-    TestCelebornShuffleManager.registerReaderGetHook(hook)
+      val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
+      val hook = new ShuffleReaderGetHook(celebornConf)
+      TestCelebornShuffleManager.registerReaderGetHook(hook)
 
-    val value = Range(1, 10000).mkString(",")
-    val tuples = sparkSession.sparkContext.parallelize(1 to 10000, 2)
-      .map { i => (i, value) }.groupByKey(16).collect()
+      val value = Range(1, 10000).mkString(",")
+      val tuples = sparkSession.sparkContext.parallelize(1 to 10000, 2)
+        .map { i => (i, value) }.groupByKey(16).collect()
 
-    // verify result
-    assert(hook.executed.get() == true)
-    assert(tuples.length == 10000)
-    for (elem <- tuples) {
-      assert(elem._2.mkString(",").equals(value))
+      // verify result
+      assert(hook.executed.get() == true)
+      assert(tuples.length == 10000)
+      for (elem <- tuples) {
+        assert(elem._2.mkString(",").equals(value))
+      }
+
+      val shuffleMgr = SparkContextHelper.env
+        .shuffleManager
+        .asInstanceOf[TestCelebornShuffleManager]
+      val lifecycleManager = shuffleMgr.getLifecycleManager
+
+      shuffleMgr.unregisterShuffle(0)
+      assert(lifecycleManager.getUnregisterShuffleTime().containsKey(0))
+      assert(lifecycleManager.getUnregisterShuffleTime().containsKey(1))
+
+      sparkSession.stop()
     }
-
-    val shuffleMgr = SparkContextHelper.env
-      .shuffleManager
-      .asInstanceOf[TestCelebornShuffleManager]
-    val lifecycleManager = shuffleMgr.getLifecycleManager
-
-    shuffleMgr.unregisterShuffle(0)
-    assert(lifecycleManager.getUnregisterShuffleTime().containsKey(0))
-    assert(lifecycleManager.getUnregisterShuffleTime().containsKey(1))
-
-    sparkSession.stop()
   }
 
   test("celeborn spark integration test - unregister shuffle with throwsFetchFailure disabled") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "false")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "false")
+        .getOrCreate()
 
-    val value = Range(1, 10000).mkString(",")
-    val tuples = sparkSession.sparkContext.parallelize(1 to 10000, 2)
-      .map { i => (i, value) }.groupByKey(16).collect()
+      val value = Range(1, 10000).mkString(",")
+      val tuples = sparkSession.sparkContext.parallelize(1 to 10000, 2)
+        .map { i => (i, value) }.groupByKey(16).collect()
 
-    // verify result
-    assert(tuples.length == 10000)
-    for (elem <- tuples) {
-      assert(elem._2.mkString(",").equals(value))
+      // verify result
+      assert(tuples.length == 10000)
+      for (elem <- tuples) {
+        assert(elem._2.mkString(",").equals(value))
+      }
+
+      val shuffleMgr = SparkContextHelper.env
+        .shuffleManager
+        .asInstanceOf[SparkShuffleManager]
+      val lifecycleManager = shuffleMgr.getLifecycleManager
+
+      shuffleMgr.unregisterShuffle(0)
+      assert(lifecycleManager.getUnregisterShuffleTime().containsKey(0))
+
+      sparkSession.stop()
     }
-
-    val shuffleMgr = SparkContextHelper.env
-      .shuffleManager
-      .asInstanceOf[SparkShuffleManager]
-    val lifecycleManager = shuffleMgr.getLifecycleManager
-
-    shuffleMgr.unregisterShuffle(0)
-    assert(lifecycleManager.getUnregisterShuffleTime().containsKey(0))
-
-    sparkSession.stop()
   }
 
   test("celeborn spark integration test - Fetch Failure with multiple shuffle data") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .config(
-        "spark.shuffle.manager",
-        "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
+        .config(
+          "spark.shuffle.manager",
+          "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
+        .getOrCreate()
 
-    val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
-    val hook = new ShuffleReaderGetHook(celebornConf)
-    TestCelebornShuffleManager.registerReaderGetHook(hook)
+      val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
+      val hook = new ShuffleReaderGetHook(celebornConf)
+      TestCelebornShuffleManager.registerReaderGetHook(hook)
 
-    import sparkSession.implicits._
+      import sparkSession.implicits._
 
-    val df1 = Seq((1, "a"), (2, "b")).toDF("id", "data").groupBy("id").count()
-    val df2 = Seq((2, "c"), (2, "d")).toDF("id", "data").groupBy("id").count()
-    val tuples = df1.hint("merge").join(df2, "id").select("*").collect()
+      val df1 = Seq((1, "a"), (2, "b")).toDF("id", "data").groupBy("id").count()
+      val df2 = Seq((2, "c"), (2, "d")).toDF("id", "data").groupBy("id").count()
+      val tuples = df1.hint("merge").join(df2, "id").select("*").collect()
 
-    // verify result
-    assert(hook.executed.get() == true)
-    val expect = "[2,1,2]"
-    assert(tuples.head.toString().equals(expect))
-    sparkSession.stop()
+      // verify result
+      assert(hook.executed.get() == true)
+      val expect = "[2,1,2]"
+      assert(tuples.head.toString().equals(expect))
+      sparkSession.stop()
+    }
   }
 
   test("celeborn spark integration test - Fetch Failure with RDD reuse") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .config(
-        "spark.shuffle.manager",
-        "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
+        .config(
+          "spark.shuffle.manager",
+          "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
+        .getOrCreate()
 
-    val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
-    val hook = new ShuffleReaderGetHook(celebornConf)
-    TestCelebornShuffleManager.registerReaderGetHook(hook)
+      val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
+      val hook = new ShuffleReaderGetHook(celebornConf)
+      TestCelebornShuffleManager.registerReaderGetHook(hook)
 
-    val sc = sparkSession.sparkContext
-    val rdd1 = sc.parallelize(0 until 10000, 3).map(v => (v, v)).groupByKey()
-    val rdd2 = sc.parallelize(0 until 10000, 2).map(v => (v, v)).groupByKey()
-    val rdd3 = rdd1.map(v => (v._2, v._1))
+      val sc = sparkSession.sparkContext
+      val rdd1 = sc.parallelize(0 until 10000, 3).map(v => (v, v)).groupByKey()
+      val rdd2 = sc.parallelize(0 until 10000, 2).map(v => (v, v)).groupByKey()
+      val rdd3 = rdd1.map(v => (v._2, v._1))
 
-    hook.executed.set(true)
+      hook.executed.set(true)
 
-    rdd1.count()
-    rdd2.count()
+      rdd1.count()
+      rdd2.count()
 
-    hook.executed.set(false)
-    rdd3.count()
-    hook.executed.set(false)
-    rdd3.count()
-    hook.executed.set(false)
-    rdd3.count()
-    hook.executed.set(false)
-    rdd3.count()
+      hook.executed.set(false)
+      rdd3.count()
+      hook.executed.set(false)
+      rdd3.count()
+      hook.executed.set(false)
+      rdd3.count()
+      hook.executed.set(false)
+      rdd3.count()
 
-    sparkSession.stop()
+      sparkSession.stop()
+    }
   }
 
   test("celeborn spark integration test - Fetch Failure with read write shuffles in one stage") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .config(
-        "spark.shuffle.manager",
-        "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
+        .config(
+          "spark.shuffle.manager",
+          "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
+        .getOrCreate()
 
-    val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
-    val hook = new ShuffleReaderGetHook(celebornConf)
-    TestCelebornShuffleManager.registerReaderGetHook(hook)
+      val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
+      val hook = new ShuffleReaderGetHook(celebornConf)
+      TestCelebornShuffleManager.registerReaderGetHook(hook)
 
-    val sc = sparkSession.sparkContext
-    val rdd1 = sc.parallelize(0 until 10000, 3).map(v => (v, v)).groupByKey()
-    val rdd2 = rdd1.map(v => (v._2, v._1)).groupByKey()
+      val sc = sparkSession.sparkContext
+      val rdd1 = sc.parallelize(0 until 10000, 3).map(v => (v, v)).groupByKey()
+      val rdd2 = rdd1.map(v => (v._2, v._1)).groupByKey()
 
-    hook.executed.set(true)
-    rdd1.count()
+      hook.executed.set(true)
+      rdd1.count()
 
-    hook.executed.set(false)
-    rdd2.count()
+      hook.executed.set(false)
+      rdd2.count()
 
-    sparkSession.stop()
+      sparkSession.stop()
+    }
   }
 
   test("celeborn spark integration test - empty shuffle data") {
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.shuffle.enabled", "true")
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
+      val sparkSession = SparkSession.builder()
+        .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
+        .config("spark.sql.shuffle.partitions", 2)
+        .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
+        .config("spark.celeborn.shuffle.enabled", "true")
+        .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
+        .getOrCreate()
 
-    sparkSession.sql("create table if not exists t_1 (a bigint) using parquet")
-    sparkSession.sql("create table if not exists t_2 (a bigint) using parquet")
-    sparkSession.sql("create table if not exists t_3 (a bigint) using parquet")
-    val df1 = sparkSession.table("t_1")
-    val df2 = sparkSession.table("t_2")
-    val df3 = sparkSession.table("t_3")
-    df1.union(df2).union(df3).count()
+      sparkSession.sql("create table if not exists t_1 (a bigint) using parquet")
+      sparkSession.sql("create table if not exists t_2 (a bigint) using parquet")
+      sparkSession.sql("create table if not exists t_3 (a bigint) using parquet")
+      val df1 = sparkSession.table("t_1")
+      val df2 = sparkSession.table("t_2")
+      val df3 = sparkSession.table("t_3")
+      df1.union(df2).union(df3).count()
 
-    sparkSession.stop()
+      sparkSession.stop()
+    }
   }
 }
