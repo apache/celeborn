13382b0d877fdb52d859c61035c22929bee3ab17
MiyueSC <913784771@qq.com>
2024-03-13 21:26:05 +0800

    Unify components naming

--- web/src/views/application/index.vue
+++ web/src/views/application/index.vue
@@ -17,8 +17,8 @@
 -->
 
 <script setup lang="ts">
-import { getApplicationList, getApplicationOverview } from '@/api'
 import type { Application } from '@/api/models/application/types'
+import { getApplicationList, getApplicationOverview } from '@/api'
 import { usePagination } from '@/composables'
 import { ApplicationTableService, ApplicationFormService } from '@/views/application/components'
 import { ApplicationOverviewService } from '@/views/overview/components'
