cc964a10739853ac5f20f9f362f85bbbc94a1607
Mridul Muralidharan <mridulatgmail.com>
2024-03-18 15:11:38 -0500

    Mark connection as timedOut in TransportClient.close

--- common/src/main/java/org/apache/celeborn/common/network/client/TransportClient.java
+++ common/src/main/java/org/apache/celeborn/common/network/client/TransportClient.java
@@ -317,6 +317,9 @@ public class TransportClient implements Closeable {
 
   @Override
   public void close() {
+    // Mark the connection as timed out, so we do not return a connection that's being closed
+    // from the TransportClientFactory if closing takes some time (e.g. with SSL)
+    this.timedOut = true;
     // close is a local operation and should finish with milliseconds; timeout just to be safe
     channel.close().awaitUninterruptibly(10, TimeUnit.SECONDS);
   }
