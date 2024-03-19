5606db1ae1ffbb7ea43e29cdf3563b5a24ef5eed
MiyueSC <913784771@qq.com>
2024-03-19 16:34:51 +0800

    Add search form

--- web/src/views/tenant/index.vue
+++ web/src/views/tenant/index.vue
@@ -20,7 +20,7 @@
 import { getTenantList, getTenantOverview } from '@/api'
 import { usePagination } from '@/composables'
 import { useHasLoading } from '@varlet/axle/use'
-import { TenantTableService, TenantOverviewService } from './components'
+import { TenantTableService, TenantOverviewService, TenantFormService } from './components'
 
 defineOptions({
   name: 'TenantView'
@@ -34,7 +34,7 @@ const {
   loading: isTableLoading
 } = getTenantList().use({ immediate: false })
 
-const { pagination } = usePagination({
+const { pagination, resetSearch, doSearch } = usePagination({
   onLoadData
 })
 
@@ -45,6 +45,7 @@ const loading = useHasLoading(isOverviewLoading, isTableLoading)
   <n-spin :show="loading">
     <n-flex :style="{ gap: '24px' }" vertical>
       <TenantOverviewService :data="overview" />
+      <TenantFormService @search="doSearch" @reset="resetSearch" />
       <TenantTableService :data="data?.tenantInfos" :pagination="pagination" />
     </n-flex>
   </n-spin>
