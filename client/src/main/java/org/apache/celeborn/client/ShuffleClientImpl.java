fb7271dc2e46bbce6b5f87ca0747f314116ebc0e
wangshengjie <wangshengjie3@xiaomi.com>
2024-03-24 17:28:00 +0800

    format code

--- client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
+++ client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
@@ -1110,8 +1110,7 @@ public class ShuffleClientImpl extends ShuffleClient {
                       nextBatchId);
                   if (dataPushFailureTrackingEnabled) {
                     pushState.addFailedBatch(
-                        latest.getUniqueId(),
-                        new PushFailedBatch(mapId, attemptId, nextBatchId));
+                        latest.getUniqueId(), new PushFailedBatch(mapId, attemptId, nextBatchId));
                   }
                   ReviveRequest reviveRequest =
                       new ReviveRequest(
@@ -1181,8 +1180,7 @@ public class ShuffleClientImpl extends ShuffleClient {
             public void onFailure(Throwable e) {
               if (dataPushFailureTrackingEnabled) {
                 pushState.addFailedBatch(
-                    latest.getUniqueId(),
-                    new PushFailedBatch(mapId, attemptId, nextBatchId));
+                    latest.getUniqueId(), new PushFailedBatch(mapId, attemptId, nextBatchId));
               }
               if (pushState.exception.get() != null) {
                 return;
@@ -1553,8 +1551,7 @@ public class ShuffleClientImpl extends ShuffleClient {
                 if (dataPushFailureTrackingEnabled) {
                   for (int i = 0; i < numBatches; i++) {
                     pushState.addFailedBatch(
-                        partitionUniqueIds[i],
-                        new PushFailedBatch(mapId, attemptId, batchIds[i]));
+                        partitionUniqueIds[i], new PushFailedBatch(mapId, attemptId, batchIds[i]));
                   }
                 }
                 ReviveRequest[] requests =
@@ -1615,8 +1612,7 @@ public class ShuffleClientImpl extends ShuffleClient {
             if (dataPushFailureTrackingEnabled) {
               for (int i = 0; i < numBatches; i++) {
                 pushState.addFailedBatch(
-                    partitionUniqueIds[i],
-                    new PushFailedBatch(mapId, attemptId, batchIds[i]));
+                    partitionUniqueIds[i], new PushFailedBatch(mapId, attemptId, batchIds[i]));
               }
             }
             if (pushState.exception.get() != null) {
