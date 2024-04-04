ead0789d1a9fb4bbaaa347685c0ab255f7264e39
Mridul Muralidharan <mridulatgmail.com>
2024-04-04 01:02:11 -0500

    Add SSLNettyRpcEnvSuite

--- common/src/main/scala/org/apache/celeborn/common/rpc/netty/NettyRpcEnv.scala
+++ common/src/main/scala/org/apache/celeborn/common/rpc/netty/NettyRpcEnv.scala
@@ -59,7 +59,8 @@ class NettyRpcEnv(
 
   private var worker: RpcEndpoint = null
 
-  private val transportContext =
+  // Visible for tests
+  private[netty] val transportContext =
     new TransportContext(transportConf, new NettyRpcHandler(dispatcher, this))
 
   private def createClientBootstraps(): java.util.List[TransportClientBootstrap] = {
