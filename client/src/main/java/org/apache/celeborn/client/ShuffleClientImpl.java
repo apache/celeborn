00b06fbbf46f1ebc16f91162ad4308fb90a98b4e
wangshengjie <wangshengjie3@xiaomi.com>
2024-04-08 23:26:10 +0800

    refactor code for batch open stream

--- client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
+++ client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
@@ -1890,6 +1890,7 @@ public class ShuffleClientImpl extends ShuffleClient {
       ArrayList<PartitionLocation> locations,
       ArrayList<PbStreamHandler> streamHandlers,
       Map<String, Set<PushFailedBatch>> failedBatchSetMap,
+      Map<String, Pair<Integer, Integer>> chunksRange,
       int[] mapAttempts,
       MetricsCallback metricsCallback)
       throws IOException {
@@ -1923,6 +1924,7 @@ public class ShuffleClientImpl extends ShuffleClient {
           streamHandlers,
           mapAttempts,
           failedBatchSetMap,
+          chunksRange,
           attemptNumber,
           startMapIndex,
           endMapIndex,
