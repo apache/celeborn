a88f972dbd0a1f432bb5a2c31d0bf936dd8b1aee
onebox-li <lyh-36@163.com>
2024-04-12 14:06:54 +0800

    [CELEBORN-1381] Avoid construct TransportConf when creating CelebornInputStream
    ### What changes were proposed in this pull request?
    Get retryWaitMs directly from conf when creating CelebornInputStream.
    
    ### Why are the changes needed?
    It is unnecessary to construct TransportConf here.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Manual test.
    
    Closes #2453 from onebox-li/CELEBORN-1381.
    
    Authored-by: onebox-li <lyh-36@163.com>
    Signed-off-by: SteNicholas <programgeek@163.com>

--- client/src/main/java/org/apache/celeborn/client/read/CelebornInputStream.java
+++ client/src/main/java/org/apache/celeborn/client/read/CelebornInputStream.java
@@ -38,7 +38,6 @@ import org.apache.celeborn.client.compress.Decompressor;
 import org.apache.celeborn.common.CelebornConf;
 import org.apache.celeborn.common.exception.CelebornIOException;
 import org.apache.celeborn.common.network.client.TransportClientFactory;
-import org.apache.celeborn.common.network.util.TransportConf;
 import org.apache.celeborn.common.protocol.*;
 import org.apache.celeborn.common.unsafe.Platform;
 import org.apache.celeborn.common.util.ExceptionMaker;
@@ -216,9 +215,7 @@ public abstract class CelebornInputStream extends InputStream {
       } else {
         fetchChunkMaxRetry = conf.clientFetchMaxRetriesForEachReplica();
       }
-      TransportConf transportConf =
-          Utils.fromCelebornConf(conf, TransportModuleConstants.DATA_MODULE, 0);
-      retryWaitMs = transportConf.ioRetryWaitTimeMs();
+      this.retryWaitMs = conf.networkIoRetryWaitMs(TransportModuleConstants.DATA_MODULE);
       this.callback = metricsCallback;
       this.exceptionMaker = exceptionMaker;
       this.partitionId = partitionId;
