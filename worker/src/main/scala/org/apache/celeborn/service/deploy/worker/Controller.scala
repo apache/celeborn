59bef4588704cd76a44759cefb785e9982b8acc1
wangshengjie <wangshengjie3@xiaomi.com>
2024-06-03 11:55:16 +0800

    fix code style check error

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Controller.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/Controller.scala
@@ -338,11 +338,12 @@ private[deploy] class Controller(
                     logDebug(s"Location $uniqueId is deleted.")
                   } else {
                     val storageInfo = fileWriter.getStorageInfo
-                    val fileInfo = if (null != fileWriter.getDiskFileInfo) {
-                      fileWriter.getDiskFileInfo
-                    } else {
-                      fileWriter.getMemoryFileInfo
-                    }
+                    val fileInfo =
+                      if (null != fileWriter.getDiskFileInfo) {
+                        fileWriter.getDiskFileInfo
+                      } else {
+                        fileWriter.getMemoryFileInfo
+                      }
                     val fileMeta = fileInfo.getFileMeta
                     fileMeta match {
                       case meta: ReduceFileMeta =>
