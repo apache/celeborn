9029601e1f0d81677d94f1d5fc396b34b3d781ad
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2024-07-11 10:39:59 +0800

    minor

--- client-spark/spark-2/src/main/scala/org/apache/spark/shuffle/celeborn/CelebornShuffleReader.scala
+++ client-spark/spark-2/src/main/scala/org/apache/spark/shuffle/celeborn/CelebornShuffleReader.scala
@@ -149,7 +149,7 @@ class CelebornShuffleReader[K, C](
       }
     }).filter {
       case (_, inputStream) => inputStream != CelebornInputStream.empty()
-    }..map { case (partitionId, inputStream) =>
+    }.map { case (partitionId, inputStream) =>
       (partitionId, serializerInstance.deserializeStream(inputStream).asKeyValueIterator)
     }.flatMap { case (partitionId, iter) =>
       try {
