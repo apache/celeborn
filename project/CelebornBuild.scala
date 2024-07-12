2eefc81c987a3a79d0fc27e0a876d7df5327261a
zhaohehuhu <luoyedeyi@163.com>
2024-07-12 10:24:28 +0800

    add hadoopAwsDependencies into CelebornBuild

--- project/CelebornBuild.scala
+++ project/CelebornBuild.scala
@@ -111,6 +111,8 @@ object Dependencies {
     ExclusionRule("jline", "jline"),
     ExclusionRule("log4j", "log4j"),
     ExclusionRule("org.slf4j", "slf4j-log4j12"))
+  val hadoopAws = "org.apache.hadoop" % "hadoop-aws" % hadoopVersion
+  val awsClient = "com.amazonaws" % "aws-java-sdk-bundle" % awsVersion
   val ioDropwizardMetricsCore = "io.dropwizard.metrics" % "metrics-core" % metricsVersion
   val ioDropwizardMetricsGraphite = "io.dropwizard.metrics" % "metrics-graphite" % metricsVersion excludeAll (
     ExclusionRule("com.rabbitmq", "amqp-client"))
@@ -443,6 +445,13 @@ object Utils {
 }
 
 object CelebornCommon {
+
+  lazy val hadoopAwsDependencies = if(profiles.filter(_.startsWith("hadoop-aws")).headOption){
+    Seq(Dependencies.hadoopAws, Dependencies.awsClient)
+  } else {
+    Seq.empty
+  }
+
   lazy val common = Project("celeborn-common", file("common"))
     .settings (
       commonSettings,
@@ -478,7 +487,7 @@ object CelebornCommon {
         // SSL support
         Dependencies.bouncycastleBcprovJdk18on,
         Dependencies.bouncycastleBcpkixJdk18on
-      ) ++ commonUnitTestDependencies,
+      ) ++ commonUnitTestDependencies ++ hadoopAwsDependencies,
 
       Compile / sourceGenerators += Def.task {
         val file = (Compile / sourceManaged).value / "org" / "apache" / "celeborn" / "package.scala"
