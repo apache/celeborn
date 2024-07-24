5f055d6b19bbfeb240f57340745a5a90864c9677
Wang, Fei <fwang12@ebay.com>
2024-07-24 10:49:01 -0700

    spi

---
+++ spi/src/main/scala/org/apache/celeborn/spi/authentication/TokenAuthenticationProvider.scala
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.celeborn.spi.authentication
+
+import java.security.Principal
+
+trait TokenAuthenticationProvider {
+
+  /**
+   * The authenticate method is called by the celeborn authentication layer
+   * to authenticate credential for their requests.
+   * If the credential is to be granted, return nothing/throw nothing.
+   * When the credential is to be disallowed, throw an appropriate [[SecurityException]].
+   *
+   * @param credential The credential received over the connection request
+   * @return The identifier associated with the token
+   *
+   * @throws SecurityException When the credential is found to be invalid by the implementation
+   */
+  @throws[SecurityException]
+  def authenticate(credential: TokenCredential): Principal
+}
