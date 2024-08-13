1d1fa0e506475ecda632be1b7e15b20737511b57
Aravind Patnam <apatnam@linkedin.com>
2024-08-12 22:25:30 -0700

    checkpoint

---
+++ cli/src/main/scala/org/apache/celeborn/CelebornCli.scala
@@ -0,0 +1,12 @@
+package org.apache.celeborn
+
+object CelebornCli {
+  def main(args: Array[String]):Unit = {
+    new CelebornCli().run(args)
+  }
+}
+
+class CelebornCli {
+  def run(args: Array[String]): Unit = {
+  }
+}
