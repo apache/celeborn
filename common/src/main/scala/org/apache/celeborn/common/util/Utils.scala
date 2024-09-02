30b61f8a66238df5527367902247ba9db7bff42b
Aravind Patnam <apatnam@linkedin.com>
2024-09-02 15:11:43 -0700

    fix ut

--- common/src/main/scala/org/apache/celeborn/common/util/Utils.scala
+++ common/src/main/scala/org/apache/celeborn/common/util/Utils.scala
@@ -26,19 +26,22 @@ import java.nio.channels.FileChannel
 import java.nio.charset.StandardCharsets
 import java.util
 import java.util.{Locale, Properties, Random, UUID}
-import java.util.concurrent.{Callable, ThreadPoolExecutor, TimeUnit, TimeoutException}
+import java.util.concurrent.{Callable, ThreadPoolExecutor, TimeoutException, TimeUnit}
+
 import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 import scala.io.Source
 import scala.reflect.ClassTag
-import scala.util.{Try, Random => ScalaRandom}
+import scala.util.{Random => ScalaRandom, Try}
 import scala.util.control.{ControlThrowable, NonFatal}
 import scala.util.matching.Regex
+
 import com.google.protobuf.{ByteString, GeneratedMessageV3}
 import io.netty.channel.unix.Errors.NativeIoException
 import org.apache.commons.lang3.SystemUtils
 import org.apache.commons.lang3.time.FastDateFormat
 import org.roaringbitmap.RoaringBitmap
+
 import org.apache.celeborn.common.CelebornConf
 import org.apache.celeborn.common.CelebornConf.PORT_MAX_RETRY
 import org.apache.celeborn.common.exception.CelebornException
@@ -417,13 +420,11 @@ object Utils extends Logging {
   }
 
   def localHostNameForAdvertiseAddress(conf: CelebornConf, env: String): String = {
-    if (env.equals(RpcNameConstants.MASTER_SYS) || env.equals(RpcNameConstants.MASTER_INTERNAL_SYS)) {
+    if (env.equals(RpcNameConstants.MASTER_SYS) || env.equals(
+        RpcNameConstants.MASTER_INTERNAL_SYS)) {
       getAdvertiseAddressForMaster(conf)
-    } else if (env.equals(RpcNameConstants.WORKER_SYS) || env.equals(RpcNameConstants.WORKER_INTERNAL_SYS)) {
-      getAdvertiseAddressForWorker(conf)
     } else {
-      throw new IllegalArgumentException(s"Unknown env to get advertise address: $env. Expected one of MASTER_SYS," +
-        s" MASTER_INTERNAL_SYS, WORKER_SYS, or WORKER_INTERNAL_SYS.")
+      getAdvertiseAddressForWorker(conf)
     }
   }
 
