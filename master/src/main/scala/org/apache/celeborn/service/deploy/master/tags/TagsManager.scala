3fc74ec06b0753da04253c5a465be7b541583446
Sanskar Modi <sanskarmodi97@gmail.com>
2024-09-27 15:20:24 +0800

    [CELEBORN-1597][CIP-11] Implement TagsManager
    ### What changes were proposed in this pull request?
    
    Added tags manager which will be responsible for managing worker tags. This will be used in the follow up PRs
    
    ### Why are the changes needed?
    
    https://cwiki.apache.org/confluence/display/CELEBORN/CIP-11+Supporting+Tags+in+Celeborn
    
    ### Does this PR introduce _any_ user-facing change?
    
    NA
    
    ### How was this patch tested?
    Added UTs
    
    Closes #2739 from s0nskar/tags_manager.
    
    Authored-by: Sanskar Modi <sanskarmodi97@gmail.com>
    Signed-off-by: mingji <fengmingxiao.fmx@alibaba-inc.com>

---
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/tags/TagsManager.scala
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.celeborn.service.deploy.master.tags
+
+import java.util
+import java.util.{Set => JSet}
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.JavaConverters.{asScalaIteratorConverter, mapAsScalaConcurrentMapConverter}
+
+import org.apache.celeborn.common.internal.Logging
+import org.apache.celeborn.common.meta.WorkerInfo
+
+class TagsManager extends Logging {
+  private val tagStore = new ConcurrentHashMap[String, JSet[String]]()
+
+  private val addNewTagFunc =
+    new util.function.Function[String, ConcurrentHashMap.KeySetView[String, java.lang.Boolean]]() {
+      override def apply(t: String): ConcurrentHashMap.KeySetView[String, java.lang.Boolean] =
+        ConcurrentHashMap.newKeySet[String]()
+    }
+
+  def getTaggedWorkers(tag: String, workers: List[WorkerInfo]): List[WorkerInfo] = {
+    val workersForTag = tagStore.get(tag)
+    if (workersForTag == null) {
+      logWarning(s"Tag $tag not found in cluster")
+      return List.empty
+    }
+    workers.filter(worker => workersForTag.contains(worker.toUniqueId()))
+  }
+
+  def addTagToWorker(tag: String, workerId: String): Unit = {
+    val workers = tagStore.computeIfAbsent(tag, addNewTagFunc)
+    logInfo(s"Adding Tag $tag to worker $workerId")
+    workers.add(workerId)
+  }
+
+  def removeTagFromWorker(tag: String, workerId: String): Unit = {
+    val workers = tagStore.get(tag)
+
+    if (workers != null && workers.contains(workerId)) {
+      logInfo(s"Removing Tag $tag from worker $workerId")
+      workers.remove(workerId)
+    } else {
+      logWarning(s"Tag $tag not found for worker $workerId")
+    }
+  }
+
+  def getTagsForWorker(worker: WorkerInfo): Set[String] = {
+    tagStore.asScala.filter(_._2.contains(worker.toUniqueId())).keySet.toSet
+  }
+
+  def removeTagFromCluster(tag: String): Unit = {
+    val workers = tagStore.remove(tag)
+    if (workers != null) {
+      logInfo(s"Removed Tag $tag from cluster with workers ${workers.toArray.mkString(", ")}")
+    } else {
+      logWarning(s"Tag $tag not found in cluster and thus can not be removed")
+    }
+  }
+
+  def getTagsForCluster: Set[String] = {
+    tagStore.keySet().iterator().asScala.toSet
+  }
+}
