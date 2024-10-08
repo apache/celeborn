867336fff1d34c479fbdfce40a7628741ca224f3
szt <zaynt4606@163.com>
2024-10-08 10:18:06 +0800

    delete useless code

--- common/src/main/scala/org/apache/celeborn/common/CelebornConf.scala
+++ common/src/main/scala/org/apache/celeborn/common/CelebornConf.scala
@@ -4684,7 +4684,7 @@ object CelebornConf extends Logging {
         "Otherwise, client just process the shuffle in general Reduce Partition mode.")
       .version("0.6.0")
       .booleanConf
-      .createWithDefault(false)
+      .createWithDefault(true)
 
   val GROUP_MAP_TASK_GROUP_SIZE: ConfigEntry[Int] =
     buildConf("celeborn.client.shuffle.groupMapTask.groupSize")
@@ -4694,7 +4694,7 @@ object CelebornConf extends Logging {
       .doc("The group size of map task when grouping map task is enable.")
       .version("0.6.0")
       .intConf
-      .createWithDefault(1000)
+      .createWithDefault(100)
 
   val CLIENT_BATCH_HANDLE_CHANGE_PARTITION_ENABLED: ConfigEntry[Boolean] =
     buildConf("celeborn.client.shuffle.batchHandleChangePartition.enabled")
