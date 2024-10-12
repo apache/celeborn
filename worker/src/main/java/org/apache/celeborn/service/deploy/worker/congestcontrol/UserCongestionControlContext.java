1d44e5fbfe0244a035ae4fa7cbf83837875323ef
Xianming Lei <31424839+leixm@users.noreply.github.com>
2024-10-12 10:17:33 +0800

    [CELEBORN-1487][PHASE1] CongestionController support control traffic by user/worker traffic speed
    ### What changes were proposed in this pull request?
    Introduce support control traffic by user/worker traffic speed.
    
    ### Why are the changes needed?
    Currently, Celeborn only supports quota management based on disk file bytes/count, and this quota management cannot cope with sudden increases in traffic, which will cause corrupt to the cluster.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes.
    
    ### How was this patch tested?
    UTs.
    
    Closes #2797 from leixm/issue_1487_1.
    
    Authored-by: Xianming Lei <31424839+leixm@users.noreply.github.com>
    Signed-off-by: Shuang <lvshuang.xjs@alibaba-inc.com>

---
+++ worker/src/main/java/org/apache/celeborn/service/deploy/worker/congestcontrol/UserCongestionControlContext.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.celeborn.service.deploy.worker.congestcontrol;
+
+import com.codahale.metrics.Gauge;
+
+import org.apache.celeborn.common.identity.UserIdentifier;
+import org.apache.celeborn.common.metrics.source.AbstractSource;
+import org.apache.celeborn.service.deploy.worker.WorkerSource;
+
+public class UserCongestionControlContext {
+
+  private volatile boolean congestionControlFlag;
+
+  private final UserBufferInfo userBufferInfo;
+
+  private final BufferStatusHub workerBufferStatusHub;
+
+  private final UserIdentifier userIdentifier;
+
+  public UserCongestionControlContext(
+      BufferStatusHub workerBufferStatusHub,
+      UserBufferInfo userBufferInfo,
+      AbstractSource workerSource,
+      UserIdentifier userIdentifier) {
+    this.congestionControlFlag = false;
+    this.userBufferInfo = userBufferInfo;
+    this.userIdentifier = userIdentifier;
+    this.workerBufferStatusHub = workerBufferStatusHub;
+    workerSource.addGauge(
+        WorkerSource.USER_PRODUCE_SPEED(),
+        userIdentifier.toJMap(),
+        (Gauge<Long>) () -> userBufferInfo.getBufferStatusHub().avgBytesPerSec());
+  }
+
+  public void onCongestionControl() {
+    congestionControlFlag = true;
+  }
+
+  public void offCongestionControl() {
+    congestionControlFlag = false;
+  }
+
+  public boolean inCongestionControl() {
+    return congestionControlFlag;
+  }
+
+  public void updateProduceBytes(long numBytes) {
+    long timeNow = System.currentTimeMillis();
+    BufferStatusHub.BufferStatusNode node = new BufferStatusHub.BufferStatusNode(numBytes);
+    userBufferInfo.updateInfo(timeNow, node);
+    workerBufferStatusHub.add(timeNow, node);
+  }
+
+  public UserBufferInfo getUserBufferInfo() {
+    return userBufferInfo;
+  }
+
+  public UserIdentifier getUserIdentifier() {
+    return userIdentifier;
+  }
+}
