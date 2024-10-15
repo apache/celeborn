b7e32f4cde5fda69684aaa0f651fbe4f90ba2298
Wang, Fei <fwang12@ebay.com>
2024-10-15 21:45:36 +0800

    [CELEBORN-1631] Support to apply ratis snapshot operation with RESTful api
    ### What changes were proposed in this pull request?
    Sub task of CELEBORN-1628
    
    ### Why are the changes needed?
    
    Mapping for ratis-shell command:
    
    ```
    $ celeborn-ratis sh snapshot create -peers <P0_HOST:P0_PORT,P1_HOST:P1_PORT,P2_HOST:P2_PORT> -peerId <peerId0> [-groupid <RAFT_GROUP_ID>]
    ```
    It is helpful for automation integration.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, it is a new API.
    
    ### How was this patch tested?
    Integration testing.
    
    <img width="1259" alt="image" src="https://github.com/user-attachments/assets/1f74a899-17f3-41b3-911a-36374bf0fd0b">
    
    Closes #2803 from turboFei/snapshot_create.
    
    Authored-by: Wang, Fei <fwang12@ebay.com>
    Signed-off-by: Shuang <lvshuang.xjs@alibaba-inc.com>

--- master/src/main/scala/org/apache/celeborn/service/deploy/master/http/api/v1/RatisResource.scala
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/http/api/v1/RatisResource.scala
@@ -25,7 +25,7 @@ import scala.collection.JavaConverters._
 import io.swagger.v3.oas.annotations.media.{Content, Schema}
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.apache.ratis.protocol.{LeaderElectionManagementRequest, RaftPeerId, TransferLeadershipRequest}
+import org.apache.ratis.protocol.{LeaderElectionManagementRequest, RaftPeerId, SnapshotManagementRequest, TransferLeadershipRequest}
 import org.apache.ratis.rpc.CallId
 
 import org.apache.celeborn.common.internal.Logging
@@ -90,6 +90,31 @@ class RatisResource extends ApiRequestContext with Logging {
   @Path("/election/resume")
   def electionResume(): HandleResponse = ensureMasterHAEnabled(master) {
     applyElectionOp(new LeaderElectionManagementRequest.Resume)
+  }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      schema = new Schema(implementation = classOf[HandleResponse]))),
+    description = "Trigger the current server to take snapshot.")
+  @POST
+  @Path("/snapshot/create")
+  def createSnapshot(): HandleResponse = ensureMasterHAEnabled(master) {
+    val request = SnapshotManagementRequest.newCreate(
+      ratisServer.getClientId,
+      ratisServer.getServer.getId,
+      ratisServer.getGroupId,
+      CallId.getAndIncrement(),
+      HARaftServer.REQUEST_TIMEOUT_MS)
+    val reply = ratisServer.getServer.snapshotManagement(request)
+    if (reply.isSuccess) {
+      new HandleResponse().success(true).message(
+        s"Successfully create snapshot at ${ratisServer.getLocalAddress}.")
+    } else {
+      new HandleResponse().success(false).message(
+        s"Failed to create snapshot at ${ratisServer.getLocalAddress}. $reply")
+    }
   }
 
   private def transferLeadership(peerAddress: String): HandleResponse = {
