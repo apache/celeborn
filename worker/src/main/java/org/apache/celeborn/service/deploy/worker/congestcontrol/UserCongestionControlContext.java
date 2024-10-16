9e32ce8d27824ad805acde34f0d8a63d4015c711
Xianming Lei <31424839+leixm@users.noreply.github.com>
2024-10-16 15:05:03 +0800

    fix

--- worker/src/main/java/org/apache/celeborn/service/deploy/worker/congestcontrol/UserCongestionControlContext.java
+++ worker/src/main/java/org/apache/celeborn/service/deploy/worker/congestcontrol/UserCongestionControlContext.java
@@ -45,7 +45,7 @@ public class UserCongestionControlContext {
     workerSource.addGauge(
         WorkerSource.USER_PRODUCE_SPEED(),
         userIdentifier.toJMap(),
-            () -> userBufferInfo.getBufferStatusHub().avgBytesPerSec());
+        () -> userBufferInfo.getBufferStatusHub().avgBytesPerSec());
   }
 
   public void onCongestionControl() {
