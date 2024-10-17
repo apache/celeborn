568e335adaaa5961592ceba85725fd874096f76e
Wang, Fei <fwang12@ebay.com>
2024-10-17 11:01:12 +0800

    [CELEBORN-1630] Support to apply ratis peer operation with RESTful api
    ### What changes were proposed in this pull request?
    
    Sub task of CELEBORN-1628.
    
    Mapping for below commands:
    ```
    $ celeborn-ratis sh peer add -peers <P0_HOST:P0_PORT,P1_HOST:P1_PORT,P2_HOST:P2_PORT> [-groupid <RAFT_GROUP_ID>] -address <P4_HOST:P4_PORT,...,PN_HOST:PN_PORT>
    ```
    
    ```
    $ celeborn-ratis sh peer remove -peers <P0_HOST:P0_PORT,P1_HOST:P1_PORT,P2_HOST:P2_PORT> [-groupid <RAFT_GROUP_ID>] -address <P0_HOST:P0_PORT,...>
    ```
    
    ```
    $ celeborn-ratis sh peer setPriority -peers <P0_HOST:P0_PORT,P1_HOST:P1_PORT,P2_HOST:P2_PORT> [-groupid <RAFT_GROUP_ID>] -addressPriority <P0_HOST:P0_PORT|PRIORITY>
    ```
    
    ### Why are the changes needed?
    
    It is more convenient to apply the ratis operation with RESTful api.
    
    ### Does this PR introduce _any_ user-facing change?
    No, new api.
    
    ### How was this patch tested?
    Integration testing. Will provide the screenshot
    
    Add:
    <img width="1619" alt="image" src="https://github.com/user-attachments/assets/ab4e24bb-3a99-40da-9972-231c9dc7c46c">
    
    Remove:
    <img width="1654" alt="image" src="https://github.com/user-attachments/assets/71133818-3259-47f5-be75-0715efe97361">
    
    Set peer priority:
    <img width="1510" alt="image" src="https://github.com/user-attachments/assets/e31b3701-71c1-46fd-872b-5227fb89f6fe">
    
    Closes #2804 from turboFei/peer_raft.
    
    Authored-by: Wang, Fei <fwang12@ebay.com>
    Signed-off-by: Shuang <lvshuang.xjs@alibaba-inc.com>

--- master/src/main/scala/org/apache/celeborn/service/deploy/master/http/api/v1/RatisResource.scala
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/http/api/v1/RatisResource.scala
@@ -17,7 +17,7 @@
 
 package org.apache.celeborn.service.deploy.master.http.api.v1
 
-import javax.ws.rs.{Consumes, Path, POST, Produces}
+import javax.ws.rs.{BadRequestException, Consumes, Path, POST, Produces}
 import javax.ws.rs.core.MediaType
 
 import scala.collection.JavaConverters._
@@ -25,11 +25,12 @@ import scala.collection.JavaConverters._
 import io.swagger.v3.oas.annotations.media.{Content, Schema}
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.apache.ratis.protocol.{LeaderElectionManagementRequest, RaftPeerId, SnapshotManagementRequest, TransferLeadershipRequest}
+import org.apache.ratis.protocol.{LeaderElectionManagementRequest, RaftClientReply, RaftPeer, SetConfigurationRequest, SnapshotManagementRequest, TransferLeadershipRequest}
 import org.apache.ratis.rpc.CallId
 
+import org.apache.celeborn.common.CelebornConf
 import org.apache.celeborn.common.internal.Logging
-import org.apache.celeborn.rest.v1.model.{HandleResponse, RatisElectionTransferRequest}
+import org.apache.celeborn.rest.v1.model.{HandleResponse, RatisElectionTransferRequest, RatisPeerAddRequest, RatisPeerRemoveRequest, RatisPeerSetPriorityRequest}
 import org.apache.celeborn.server.common.http.api.ApiRequestContext
 import org.apache.celeborn.service.deploy.master.Master
 import org.apache.celeborn.service.deploy.master.clustermeta.ha.{HAMasterMetaManager, HARaftServer}
@@ -97,6 +98,123 @@ class RatisResource extends ApiRequestContext with Logging {
     content = Array(new Content(
       mediaType = MediaType.APPLICATION_JSON,
       schema = new Schema(implementation = classOf[HandleResponse]))),
+    description = "Add new peers to the raft group.")
+  @POST
+  @Path("/peer/add")
+  def peerAdd(request: RatisPeerAddRequest): HandleResponse =
+    ensureLeaderElectionMemberMajorityAddEnabled(master) {
+      if (request.getPeers.isEmpty) {
+        throw new BadRequestException("No peers specified.")
+      }
+
+      val groupInfo = ratisServer.getGroupInfo
+
+      val remaining = getRaftPeers()
+      val adding = request.getPeers.asScala.map { peer =>
+        if (remaining.exists(e =>
+            e.getId.toString == peer.getId || e.getAddress == peer.getAddress)) {
+          throw new IllegalArgumentException(
+            s"Peer $peer with same id or address already exists in group $groupInfo.")
+        }
+        RaftPeer.newBuilder()
+          .setId(peer.getId)
+          .setAddress(peer.getAddress)
+          .setPriority(0)
+          .build()
+      }
+
+      val peers = (remaining ++ adding).distinct
+
+      logInfo(s"Adding peers: $adding to group $groupInfo.")
+      logInfo(s"New peers: $peers")
+
+      val reply = setConfiguration(peers)
+      if (reply.isSuccess) {
+        new HandleResponse().success(true).message(
+          s"Successfully added peers $adding to group $groupInfo.")
+      } else {
+        new HandleResponse().success(false).message(
+          s"Failed to add peers $adding to group $groupInfo. $reply")
+      }
+    }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      schema = new Schema(implementation = classOf[HandleResponse]))),
+    description = "Remove peers from the raft group.")
+  @POST
+  @Path("/peer/remove")
+  def peerRemove(request: RatisPeerRemoveRequest): HandleResponse =
+    ensureLeaderElectionMemberMajorityAddEnabled(master) {
+      if (request.getPeers.isEmpty) {
+        throw new BadRequestException("No peers specified.")
+      }
+
+      val groupInfo = ratisServer.getGroupInfo
+
+      val removing = request.getPeers.asScala.map { peer =>
+        getRaftPeers().find { raftPeer =>
+          raftPeer.getId.toString == peer.getId && raftPeer.getAddress == peer.getAddress
+        }.getOrElse(throw new IllegalArgumentException(
+          s"Peer $peer not found in group $groupInfo."))
+      }
+      val remaining = getRaftPeers().filterNot(removing.contains)
+
+      logInfo(s"Removing peers:$removing from group $groupInfo.")
+      logInfo(s"New peers: $remaining")
+
+      val reply = setConfiguration(remaining)
+      if (reply.isSuccess) {
+        new HandleResponse().success(true).message(
+          s"Successfully removed peers $removing from group $groupInfo.")
+      } else {
+        new HandleResponse().success(false).message(
+          s"Failed to remove peers $removing from group $groupInfo. $reply")
+      }
+    }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      schema = new Schema(implementation = classOf[HandleResponse]))),
+    description = "Set the priority of the peers in the raft group.")
+  @POST
+  @Path("/peer/set_priority")
+  def peerSetPriority(request: RatisPeerSetPriorityRequest): HandleResponse =
+    ensureLeaderElectionMemberMajorityAddEnabled(master) {
+      if (request.getAddressPriorities.isEmpty) {
+        throw new BadRequestException("No peer priorities specified.")
+      }
+
+      val peers = getRaftPeers().map { peer =>
+        val newPriority = request.getAddressPriorities.get(peer.getAddress)
+        val priority: Int = if (newPriority != null) newPriority else peer.getPriority
+        RaftPeer.newBuilder(peer).setPriority(priority).build()
+      }
+
+      val peerPriorities =
+        request.getAddressPriorities.asScala.map { case (a, p) => s"$a:$p" }.mkString(", ")
+      logInfo(s"Setting peer priorities: $peerPriorities.")
+      logInfo(s"New peers: $peers")
+
+      val reply = setConfiguration(peers)
+      if (reply.isSuccess) {
+        new HandleResponse().success(true).message(
+          s"Successfully set peer priorities: $peerPriorities.")
+      } else {
+        new HandleResponse().success(false).message(
+          s"Failed to set peer priorities: $peerPriorities. $reply")
+      }
+    }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      schema = new Schema(implementation = classOf[HandleResponse]))),
     description = "Trigger the current server to take snapshot.")
   @POST
   @Path("/snapshot/create")
@@ -118,7 +236,11 @@ class RatisResource extends ApiRequestContext with Logging {
   }
 
   private def transferLeadership(peerAddress: String): HandleResponse = {
-    val newLeaderId = Option(peerAddress).map(getRaftPeerId).orNull
+    val newLeaderId = Option(peerAddress).map { addr =>
+      getRaftPeers().find(_.getAddress == addr).map(_.getId).getOrElse(
+        throw new IllegalArgumentException(
+          s"Peer $addr not found in group ${ratisServer.getGroupInfo}."))
+    }.orNull
     val op =
       if (newLeaderId == null) s"step down leader ${ratisServer.getLocalAddress}"
       else s"transfer leadership from ${ratisServer.getLocalAddress} to $peerAddress"
@@ -154,11 +276,26 @@ class RatisResource extends ApiRequestContext with Logging {
     }
   }
 
-  private def getRaftPeerId(peerAddress: String): RaftPeerId = {
-    val groupInfo =
-      master.statusSystem.asInstanceOf[HAMasterMetaManager].getRatisServer.getGroupInfo
-    groupInfo.getCommitInfos.asScala.filter(peer => peer.getServer.getAddress == peerAddress)
-      .map(peer => RaftPeerId.valueOf(peer.getServer.getId)).headOption.getOrElse(
-        throw new IllegalArgumentException(s"Peer $peerAddress not found in group: $groupInfo"))
+  private def setConfiguration(peers: Seq[RaftPeer]): RaftClientReply = {
+    ratisServer.getServer.setConfiguration(new SetConfigurationRequest(
+      ratisServer.getClientId,
+      ratisServer.getServer.getId,
+      ratisServer.getGroupId,
+      CallId.getAndIncrement(),
+      SetConfigurationRequest.Arguments.newBuilder.setServersInNewConf(peers.asJava).build()))
+  }
+
+  private def getRaftPeers(): Seq[RaftPeer] = {
+    ratisServer.getGroupInfo.getGroup.getPeers.asScala.toSeq
+  }
+
+  private def ensureLeaderElectionMemberMajorityAddEnabled[T](master: Master)(f: => T): T = {
+    ensureMasterIsLeader(master) {
+      if (!master.conf.hasMasterRatisLeaderElectionMemberMajorityAdd) {
+        throw new BadRequestException(s"This operation can only be done when" +
+          s" ${CelebornConf.HA_MASTER_RATIS_LEADER_ELECTION_MEMBER_MAJORITY_ADD.key} is true.")
+      }
+      f
+    }
   }
 }
