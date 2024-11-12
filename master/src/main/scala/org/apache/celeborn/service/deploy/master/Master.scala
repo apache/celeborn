68ddf1269db39a77d05397b910b522a375412076
xzh <953396112@qq.com>
2024-11-12 21:00:08 +0800

    [CELEBORN-1714] Optimize handleApplicationLost

--- master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
@@ -625,15 +625,7 @@ private[celeborn] class Master(
       if (heartbeatTime < currentTime - appHeartbeatTimeoutMs) {
         logWarning(s"Application $appId timeout, trigger applicationLost event.")
         val requestId = MasterClient.genRequestId()
-        var res = self.askSync[ApplicationLostResponse](ApplicationLost(appId, requestId))
-        var retry = 1
-        while (res.status != StatusCode.SUCCESS && retry <= 3) {
-          res = self.askSync[ApplicationLostResponse](ApplicationLost(appId, requestId))
-          retry += 1
-        }
-        if (retry > 3) {
-          logWarning(s"Handle ApplicationLost event for $appId failed more than 3 times!")
-        }
+        handleApplicationLost(null, appId, requestId)
       }
     }
   }
@@ -1050,7 +1042,9 @@ private[celeborn] class Master(
         if (hasHDFSStorage || hasS3Storage) {
           checkAndCleanExpiredAppDirsOnDFS(appId)
         }
-        context.reply(ApplicationLostResponse(StatusCode.SUCCESS))
+        if (context != null) {
+          context.reply(ApplicationLostResponse(StatusCode.SUCCESS))
+        }
       }
     })
   }
