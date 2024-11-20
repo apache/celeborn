c45cb7b837ea2880f94d58738a6057dc397c6706
Weijie Guo <reswqa@163.com>
2024-11-20 12:09:49 +0800

    fixup! [CELEBORN-1726] Update WorkerInfo when transition worker state

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/WorkerStatusManager.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/WorkerStatusManager.scala
@@ -129,7 +129,7 @@ private[celeborn] class WorkerStatusManager(conf: CelebornConf) extends Logging 
     if (allowStates != null && allowStates.contains(state)) {
       logInfo(s"Worker transition status from ${currentWorkerStatus.getState} to $state.")
       currentWorkerStatus = new WorkerStatus(state.getNumber, System.currentTimeMillis())
-      if (worker != null) {
+      if (worker != null & worker.workerInfo != null) {
         worker.workerInfo.setWorkerStatus(currentWorkerStatus)
       }
     } else {
