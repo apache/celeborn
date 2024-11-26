2ec8522901d6719fe377f75b9d6ac92cbacd09f2
Wang, Fei <fwang12@ebay.com>
2024-11-25 19:55:50 -0800

    remove

--- openapi/openapi-client/src/main/java/org/apache/celeborn/rest/v1/master/ApplicationApi.java
+++ openapi/openapi-client/src/main/java/org/apache/celeborn/rest/v1/master/ApplicationApi.java
@@ -25,7 +25,6 @@ import org.apache.celeborn.rest.v1.master.invoker.BaseApi;
 import org.apache.celeborn.rest.v1.master.invoker.Configuration;
 import org.apache.celeborn.rest.v1.master.invoker.Pair;
 
-import org.apache.celeborn.rest.v1.model.AppDiskUsageSnapshotsResponse;
 import org.apache.celeborn.rest.v1.model.ApplicationsHeartbeatResponse;
 import org.apache.celeborn.rest.v1.model.DeleteAppsRequest;
 import org.apache.celeborn.rest.v1.model.HandleResponse;
@@ -237,73 +236,6 @@ public class ApplicationApi extends BaseApi {
     String[] localVarAuthNames = new String[] { "basic" };
 
     TypeReference<ApplicationsHeartbeatResponse> localVarReturnType = new TypeReference<ApplicationsHeartbeatResponse>() {};
-    return apiClient.invokeAPI(
-        localVarPath,
-        "GET",
-        localVarQueryParams,
-        localVarCollectionQueryParams,
-        localVarQueryStringJoiner.toString(),
-        localVarPostBody,
-        localVarHeaderParams,
-        localVarCookieParams,
-        localVarFormParams,
-        localVarAccept,
-        localVarContentType,
-        localVarAuthNames,
-        localVarReturnType
-    );
-  }
-
-  /**
-   * 
-   * List the top disk usage application ids. It will return the top disk usage application ids for the cluster. 
-   * @return AppDiskUsageSnapshotsResponse
-   * @throws ApiException if fails to make API call
-   */
-  public AppDiskUsageSnapshotsResponse getApplicationsDiskUsageSnapshots() throws ApiException {
-    return this.getApplicationsDiskUsageSnapshots(Collections.emptyMap());
-  }
-
-
-  /**
-   * 
-   * List the top disk usage application ids. It will return the top disk usage application ids for the cluster. 
-   * @param additionalHeaders additionalHeaders for this call
-   * @return AppDiskUsageSnapshotsResponse
-   * @throws ApiException if fails to make API call
-   */
-  public AppDiskUsageSnapshotsResponse getApplicationsDiskUsageSnapshots(Map<String, String> additionalHeaders) throws ApiException {
-    Object localVarPostBody = null;
-    
-    // create path and map variables
-    String localVarPath = "/api/v1/applications/top_disk_usages";
-
-    StringJoiner localVarQueryStringJoiner = new StringJoiner("&");
-    String localVarQueryParameterBaseName;
-    List<Pair> localVarQueryParams = new ArrayList<Pair>();
-    List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
-    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
-    Map<String, String> localVarCookieParams = new HashMap<String, String>();
-    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
-
-    
-    localVarHeaderParams.putAll(additionalHeaders);
-
-    
-    
-    final String[] localVarAccepts = {
-      "application/json"
-    };
-    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
-
-    final String[] localVarContentTypes = {
-      
-    };
-    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
-
-    String[] localVarAuthNames = new String[] { "basic" };
-
-    TypeReference<AppDiskUsageSnapshotsResponse> localVarReturnType = new TypeReference<AppDiskUsageSnapshotsResponse>() {};
     return apiClient.invokeAPI(
         localVarPath,
         "GET",
