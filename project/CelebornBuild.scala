d93af838ae5319b14f08e1a86ddc8a6406dde4fd
SteNicholas <programgeek@163.com>
2024-12-26 16:24:17 +0800

    [CELEBORN-1806] Bump Spark from 3.5.3 to 3.5.4

--- project/CelebornBuild.scala
+++ project/CelebornBuild.scala
@@ -808,7 +808,7 @@ object Spark35 extends SparkClientProjects {
   val lz4JavaVersion = "1.8.0"
   val sparkProjectScalaVersion = "2.12.18"
 
-  val sparkVersion = "3.5.3"
+  val sparkVersion = "3.5.4"
   val zstdJniVersion = "1.5.5-4"
 
   override val sparkColumnarShuffleVersion: String = "3.5"
