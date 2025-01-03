8b096ea8798baaad016cd6dbb7ec59caab7240de
HolyLow <jiaming.xie7@gmail.com>
2025-01-03 16:18:50 +0800

    [CELEBORN-1814][CIP-14] Add transportMessage to cppClient
    ### What changes were proposed in this pull request?
    Add transportMessage to cppClient.
    
    ### Why are the changes needed?
    TransportMessage is the building block of controlMessages.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Compilation and UTs.
    
    Closes #3042 from HolyLow/issue/celeborn-1814-add-transport-message-to-cppClient.
    
    Authored-by: HolyLow <jiaming.xie7@gmail.com>
    Signed-off-by: mingji <fengmingxiao.fmx@alibaba-inc.com>

---
+++ cpp/celeborn/protocol/TransportMessage.cpp
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "celeborn/protocol/TransportMessage.h"
+#include "celeborn/utils/Exceptions.h"
+
+namespace celeborn {
+namespace protocol {
+TransportMessage::TransportMessage(MessageType type, std::string&& payload)
+    : type_(type), payload_(std::move(payload)) {
+  messageTypeValue_ = type;
+}
+
+TransportMessage::TransportMessage(std::unique_ptr<ReadOnlyByteBuffer> buf) {
+  int messageTypeValue = buf->read<int32_t>();
+  int payloadLen = buf->read<int32_t>();
+  CELEBORN_CHECK_EQ(buf->remainingSize(), payloadLen);
+  CELEBORN_CHECK(MessageType_IsValid(messageTypeValue));
+  type_ = static_cast<MessageType>(messageTypeValue);
+  messageTypeValue_ = type_;
+  payload_ = buf->readToString(payloadLen);
+}
+
+std::unique_ptr<ReadOnlyByteBuffer> TransportMessage::toReadOnlyByteBuffer()
+    const {
+  int bufSize = payload_.size() + 4 + 4;
+  auto buffer = ByteBuffer::createWriteOnly(bufSize);
+  buffer->write<int>(messageTypeValue_);
+  buffer->write<int>(payload_.size());
+  buffer->writeFromString(payload_);
+  CELEBORN_CHECK_EQ(buffer->size(), bufSize);
+  return ByteBuffer::toReadOnly(std::move(buffer));
+}
+} // namespace protocol
+} // namespace celeborn
