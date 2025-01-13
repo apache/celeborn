354515df150c9db1ab99b3fc01d14fac6d99440a
Xianming Lei <31424839+leixm@users.noreply.github.com>
2025-01-13 17:35:38 +0800

    fix

--- tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/memory/MemorySparkTestBase.scala
+++ tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/memory/MemorySparkTestBase.scala
@@ -34,8 +34,7 @@ trait MemorySparkTestBase extends AnyFunSuite
 
   override def beforeAll(): Unit = {
     logInfo("test initialized , setup Celeborn mini cluster")
-    val workerConfs =
-      Map("celeborn.worker.directMemoryRatioForMemoryFileStorage" -> "0.2")
+    val workerConfs = Map("celeborn.worker.directMemoryRatioForMemoryFileStorage" -> "0.2")
     setupMiniClusterWithRandomPorts(workerConf = workerConfs, workerNum = 5)
   }
 
