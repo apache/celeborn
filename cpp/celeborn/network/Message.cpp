35a14d24697d3332eb359606fe249c2ecaeb447c
HolyLow <jiaming.xie7@gmail.com>
2025-01-18 12:52:32 +0800

    [CELEBORN-1836][CIP-14] Add Message to cppClient
    ### What changes were proposed in this pull request?
    This PR adds Message implementation to cppClient.
    
    ### Why are the changes needed?
    The Message is the data structure that transfers between network stack layers.
    The decode/encode methods are supported and are compatible with existing java implementation.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Compilation and UTs.
    
    Closes #3066 from HolyLow/issue/celeborn-1836-add-message-to-cpp-client.
    
    Authored-by: HolyLow <jiaming.xie7@gmail.com>
    Signed-off-by: Shuang <lvshuang.xjs@alibaba-inc.com>

---
+++ cpp/celeborn/network/Message.cpp
@@ -0,0 +1,153 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "celeborn/network/Message.h"
+
+namespace celeborn {
+namespace network {
+Message::Type Message::decodeType(uint8_t typeId) {
+  switch (typeId) {
+    case 0:
+      return CHUNK_FETCH_REQUEST;
+    case 1:
+      return CHUNK_FETCH_SUCCESS;
+    case 2:
+      return CHUNK_FETCH_FAILURE;
+    case 3:
+      return RPC_REQUEST;
+    case 4:
+      return RPC_RESPONSE;
+    case 5:
+      return RPC_FAILURE;
+    case 6:
+      return OPEN_STREAM;
+    case 7:
+      return STREAM_HANDLE;
+    case 9:
+      return ONE_WAY_MESSAGE;
+    case 11:
+      return PUSH_DATA;
+    case 12:
+      return PUSH_MERGED_DATA;
+    case 13:
+      return REGION_START;
+    case 14:
+      return REGION_FINISH;
+    case 15:
+      return PUSH_DATA_HAND_SHAKE;
+    case 16:
+      return READ_ADD_CREDIT;
+    case 17:
+      return READ_DATA;
+    case 18:
+      return OPEN_STREAM_WITH_CREDIT;
+    case 19:
+      return BACKLOG_ANNOUNCEMENT;
+    case 20:
+      return TRANSPORTABLE_ERROR;
+    case 21:
+      return BUFFER_STREAM_END;
+    case 22:
+      return HEARTBEAT;
+    default:
+      CELEBORN_FAIL("Unknown message type " + std::to_string(typeId));
+  }
+}
+
+std::atomic<long> Message::currRequestId_ = 0;
+
+std::unique_ptr<memory::ReadOnlyByteBuffer> Message::encode() const {
+  int bodyLength = body_->remainingSize();
+  int encodedLength = internalEncodedLength();
+  int headerLength =
+      sizeof(int32_t) + sizeof(uint8_t) + sizeof(int32_t) + encodedLength;
+  auto buffer = memory::ByteBuffer::createWriteOnly(headerLength);
+  buffer->write<int32_t>(encodedLength);
+  buffer->write<uint8_t>(type_);
+  buffer->write<int32_t>(bodyLength);
+  internalEncodeTo(*buffer);
+  auto header = memory::ByteBuffer::toReadOnly(std::move(buffer));
+  auto combinedFrame = memory::ByteBuffer::concat(*header, *body_);
+  return std::move(combinedFrame);
+}
+
+std::unique_ptr<Message> Message::decodeFrom(
+    std::unique_ptr<memory::ReadOnlyByteBuffer>&& data) {
+  int32_t encodedLength = data->read<int32_t>();
+  uint8_t typeId = data->read<uint8_t>();
+  int32_t bodyLength = data->read<int32_t>();
+  CELEBORN_CHECK_EQ(encodedLength + bodyLength, data->remainingSize());
+  Type type = decodeType(typeId);
+  switch (type) {
+    case RPC_RESPONSE:
+      return RpcResponse::decodeFrom(std::move(data));
+    case RPC_FAILURE:
+      return RpcFailure::decodeFrom(std::move(data));
+    case CHUNK_FETCH_SUCCESS:
+      return ChunkFetchSuccess::decodeFrom(std::move(data));
+    case CHUNK_FETCH_FAILURE:
+      return ChunkFetchFailure::decodeFrom(std::move(data));
+    default:
+      CELEBORN_FAIL("unsupported Message decode type " + std::to_string(type));
+  }
+}
+
+int RpcRequest::internalEncodedLength() const {
+  return sizeof(long) + sizeof(int32_t);
+}
+
+void RpcRequest::internalEncodeTo(memory::WriteOnlyByteBuffer& buffer) const {
+  buffer.write<long>(requestId_);
+  buffer.write<int32_t>(body_->remainingSize());
+}
+
+std::unique_ptr<RpcResponse> RpcResponse::decodeFrom(
+    std::unique_ptr<memory::ReadOnlyByteBuffer>&& data) {
+  long requestId = data->read<long>();
+  data->skip(4);
+  auto result = std::make_unique<RpcResponse>(requestId, std::move(data));
+  return result;
+}
+
+std::unique_ptr<RpcFailure> RpcFailure::decodeFrom(
+    std::unique_ptr<memory::ReadOnlyByteBuffer>&& data) {
+  long requestId = data->read<long>();
+  int strLen = data->read<int>();
+  CELEBORN_CHECK_EQ(data->remainingSize(), strLen);
+  std::string errorString = data->readToString(strLen);
+  return std::make_unique<RpcFailure>(requestId, std::move(errorString));
+}
+
+std::unique_ptr<ChunkFetchSuccess> ChunkFetchSuccess::decodeFrom(
+    std::unique_ptr<memory::ReadOnlyByteBuffer>&& data) {
+  protocol::StreamChunkSlice streamChunkSlice =
+      protocol::StreamChunkSlice::decodeFrom(*data);
+  return std::make_unique<ChunkFetchSuccess>(streamChunkSlice, std::move(data));
+}
+
+std::unique_ptr<ChunkFetchFailure> ChunkFetchFailure::decodeFrom(
+    std::unique_ptr<memory::ReadOnlyByteBuffer>&& data) {
+  protocol::StreamChunkSlice streamChunkSlice =
+      protocol::StreamChunkSlice::decodeFrom(*data);
+  int strLen = data->read<int>();
+  CELEBORN_CHECK_EQ(data->remainingSize(), strLen);
+  std::string errorString = data->readToString(strLen);
+  return std::make_unique<ChunkFetchFailure>(
+      streamChunkSlice, std::move(errorString));
+}
+} // namespace network
+} // namespace celeborn
---
+++ cpp/celeborn/network/tests/MessageTest.cpp
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "celeborn/network/Message.h"
+
+using namespace celeborn;
+using namespace celeborn::network;
+
+TEST(MessageTest, encodeRpcRequest) {
+  const std::string body = "test-body";
+  auto bodyBuffer = memory::ByteBuffer::createWriteOnly(body.size());
+  bodyBuffer->writeFromString(body);
+  const long requestId = 1000;
+  auto rpcRequest = std::make_unique<RpcRequest>(
+      requestId, memory::ByteBuffer::toReadOnly(std::move(bodyBuffer)));
+
+  auto encodedBuffer = rpcRequest->encode();
+  EXPECT_EQ(encodedBuffer->read<int32_t>(), sizeof(long) + sizeof(int32_t));
+  EXPECT_EQ(encodedBuffer->read<uint8_t>(), Message::Type::RPC_REQUEST);
+  EXPECT_EQ(encodedBuffer->read<int32_t>(), body.size());
+  EXPECT_EQ(encodedBuffer->read<long>(), requestId);
+  EXPECT_EQ(encodedBuffer->read<int32_t>(), body.size());
+  EXPECT_EQ(encodedBuffer->readToString(body.size()), body);
+}
+
+TEST(MessageTest, decodeRpcResponse) {
+  const std::string body = "test-body";
+  const long requestId = 1000;
+  const int headerLength = sizeof(int32_t) + sizeof(uint8_t) + sizeof(int32_t);
+  const int encodedLength = sizeof(long) + 4;
+  const int bodyLength = body.size();
+  size_t size = headerLength + encodedLength + bodyLength;
+  auto writeBuffer = memory::ByteBuffer::createWriteOnly(size);
+  writeBuffer->write<int32_t>(encodedLength);
+  writeBuffer->write<uint8_t>(Message::Type::RPC_RESPONSE);
+  writeBuffer->write<int32_t>(bodyLength);
+  writeBuffer->write<long>(requestId);
+  writeBuffer->write<int32_t>(bodyLength);
+  writeBuffer->writeFromString(body);
+  auto message = Message::decodeFrom(
+      memory::ByteBuffer::toReadOnly(std::move(writeBuffer)));
+  EXPECT_EQ(message->type(), Message::Type::RPC_RESPONSE);
+  auto rpcResponse = dynamic_cast<RpcResponse*>(message.get());
+  EXPECT_EQ(rpcResponse->requestId(), requestId);
+  auto rpcResponseBody = rpcResponse->body();
+  EXPECT_EQ(rpcResponseBody->remainingSize(), body.size());
+  EXPECT_EQ(rpcResponseBody->readToString(body.size()), body);
+}
+
+TEST(MessageTest, decodeRpcFailure) {
+  const std::string failureMsg = "test-failure-msg";
+  const long requestId = 1000;
+  const int headerLength = sizeof(int32_t) + sizeof(uint8_t) + sizeof(int32_t);
+  const int encodedLength = sizeof(long) + sizeof(int);
+  const int failureMsgLength = failureMsg.size();
+  size_t size = headerLength + encodedLength + failureMsgLength;
+  auto writeBuffer = memory::ByteBuffer::createWriteOnly(size);
+  writeBuffer->write<int32_t>(encodedLength);
+  writeBuffer->write<uint8_t>(Message::Type::RPC_FAILURE);
+  writeBuffer->write<int32_t>(failureMsgLength);
+  writeBuffer->write<long>(requestId);
+  writeBuffer->write<int32_t>(failureMsgLength);
+  writeBuffer->writeFromString(failureMsg);
+  auto message = Message::decodeFrom(
+      memory::ByteBuffer::toReadOnly(std::move(writeBuffer)));
+  EXPECT_EQ(message->type(), Message::Type::RPC_FAILURE);
+  auto rpcFailure = dynamic_cast<RpcFailure*>(message.get());
+  EXPECT_EQ(rpcFailure->requestId(), requestId);
+  auto rpcFailureBody = rpcFailure->body();
+  EXPECT_EQ(rpcFailureBody->remainingSize(), 0);
+  EXPECT_EQ(rpcFailure->errorMsg(), failureMsg);
+}
+
+TEST(MessageTest, decodeChunkFetchSuccess) {
+  const long streamId = 1000;
+  const int chunkIndex = 1001;
+  const int offset = 1002;
+  const int len = 1003;
+  const std::string body = "test-body";
+  const int headerLength = sizeof(int32_t) + sizeof(uint8_t) + sizeof(int32_t);
+  const int encodedLength =
+      sizeof(long) + sizeof(int) + sizeof(int) + sizeof(int);
+  const int bodyLength = body.size();
+  size_t size = headerLength + encodedLength + bodyLength;
+  auto writeBuffer = memory::ByteBuffer::createWriteOnly(size);
+  writeBuffer->write<int32_t>(encodedLength);
+  writeBuffer->write<uint8_t>(Message::Type::CHUNK_FETCH_SUCCESS);
+  writeBuffer->write<int32_t>(bodyLength);
+  writeBuffer->write<long>(streamId);
+  writeBuffer->write<int>(chunkIndex);
+  writeBuffer->write<int>(offset);
+  writeBuffer->write<int>(len);
+  writeBuffer->writeFromString(body);
+  auto message = Message::decodeFrom(
+      memory::ByteBuffer::toReadOnly(std::move(writeBuffer)));
+  EXPECT_EQ(message->type(), Message::Type::CHUNK_FETCH_SUCCESS);
+  auto chunkFetchSuccess = dynamic_cast<ChunkFetchSuccess*>(message.get());
+  auto streamChunkSlice = chunkFetchSuccess->streamChunkSlice();
+  EXPECT_EQ(streamChunkSlice.streamId, streamId);
+  EXPECT_EQ(streamChunkSlice.chunkIndex, chunkIndex);
+  EXPECT_EQ(streamChunkSlice.offset, offset);
+  EXPECT_EQ(streamChunkSlice.len, len);
+  auto chunkFetchSuccessBody = chunkFetchSuccess->body();
+  EXPECT_EQ(chunkFetchSuccessBody->remainingSize(), body.size());
+  EXPECT_EQ(chunkFetchSuccessBody->readToString(body.size()), body);
+}
+
+TEST(MessageTest, decodeChunkFetchFailure) {
+  const long streamId = 1000;
+  const int chunkIndex = 1001;
+  const int offset = 1002;
+  const int len = 1003;
+  const std::string failureMsg = "test-failure-msg";
+  const int headerLength = sizeof(int32_t) + sizeof(uint8_t) + sizeof(int32_t);
+  const int encodedLength =
+      sizeof(long) + sizeof(int) + sizeof(int) + sizeof(int) + sizeof(int);
+  const int failureMsgLength = failureMsg.size();
+  size_t size = headerLength + encodedLength + failureMsgLength;
+  auto writeBuffer = memory::ByteBuffer::createWriteOnly(size);
+  writeBuffer->write<int32_t>(encodedLength);
+  writeBuffer->write<uint8_t>(Message::Type::CHUNK_FETCH_FAILURE);
+  writeBuffer->write<int32_t>(failureMsgLength);
+  writeBuffer->write<long>(streamId);
+  writeBuffer->write<int>(chunkIndex);
+  writeBuffer->write<int>(offset);
+  writeBuffer->write<int>(len);
+  writeBuffer->write<int>(failureMsgLength);
+  writeBuffer->writeFromString(failureMsg);
+  auto message = Message::decodeFrom(
+      memory::ByteBuffer::toReadOnly(std::move(writeBuffer)));
+  EXPECT_EQ(message->type(), Message::Type::CHUNK_FETCH_FAILURE);
+  auto chunkFetchFailure = dynamic_cast<ChunkFetchFailure*>(message.get());
+  auto streamChunkSlice = chunkFetchFailure->streamChunkSlice();
+  EXPECT_EQ(streamChunkSlice.streamId, streamId);
+  EXPECT_EQ(streamChunkSlice.chunkIndex, chunkIndex);
+  EXPECT_EQ(streamChunkSlice.offset, offset);
+  EXPECT_EQ(streamChunkSlice.len, len);
+  EXPECT_EQ(chunkFetchFailure->errorMsg(), failureMsg);
+}
