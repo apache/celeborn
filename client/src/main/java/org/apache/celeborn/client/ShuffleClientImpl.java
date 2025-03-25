8ac7edefc9ceb1292c3667c5d3096cae369d9ccd
Angerszhuuuu <angers.zhu@gmail.com>
2025-03-25 17:42:00 +0800

    Update ShuffleClientImpl.java

--- client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
+++ client/src/main/java/org/apache/celeborn/client/ShuffleClientImpl.java
@@ -1544,8 +1544,9 @@ public class ShuffleClientImpl extends ShuffleClient {
                     dataBatchReviveInfos);
               } else {
                 callback.onFailure(
-                    new CelebornIOException("Parser pushMergedData response failed. " +
-                        "Current client can't be compatibility with older worker version."));
+                    new CelebornIOException(
+                        "Parser pushMergedData response failed. " +
+                              "Current client can't be compatibility with older worker version."));
                 return;
               }
               if (batchesNeedResubmit.isEmpty()) {
