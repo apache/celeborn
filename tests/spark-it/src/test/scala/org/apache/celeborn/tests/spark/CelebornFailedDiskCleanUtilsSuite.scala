5d8ade0b029b208cc92f87f1039d411bb1d33f84
CodingCat <zhunansjtu@gmail.com>
2025-04-30 20:42:14 -0700

    merge

--- tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFailedDiskCleanUtilsSuite.scala
+++ tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFailedDiskCleanUtilsSuite.scala
@@ -16,8 +16,6 @@
  */
 package org.apache.celeborn.tests.spark
 
-import org.apache.spark.SparkEnv
-import org.apache.spark.shuffle.celeborn.SparkShuffleManager
 import org.scalatest.concurrent.Eventually.eventually
 import org.scalatest.concurrent.Futures.{interval, timeout}
 import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
@@ -25,44 +23,39 @@ import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 import org.apache.celeborn.spark.FailedShuffleCleaner
 import org.apache.celeborn.tests.spark.fetch_failure.FetchFailureDiskCleanBase
 
-class CelebornFailedDiskCleanUtilsSuite extends SparkTestBase {
+class CelebornFailedDiskCleanUtilsSuite extends FetchFailureDiskCleanBase {
   test("test correctness of RunningStageManager") {
-    System.clearProperty(FailedShuffleCleaner.RUNNING_STAGE_CHECKER_CLASS)
-    val sparkConf = new SparkConf().setAppName("rss-demo").setMaster("local[2,3]")
-    val sparkSession = SparkSession.builder()
-      .config(updateSparkConf(sparkConf, ShuffleMode.HASH))
-      .config("spark.sql.shuffle.partitions", 2)
-      .config("spark.celeborn.shuffle.forceFallback.partition.enabled", false)
-      .config("spark.celeborn.client.spark.fetch.throwsFetchFailure", "true")
-      .config(
-        "spark.shuffle.manager",
-        "org.apache.spark.shuffle.celeborn.TestCelebornShuffleManager")
-      .getOrCreate()
+    if (Spark3OrNewer) {
+      val sparkSession = createSparkSession(enableFailedShuffleCleaner = true)
+      sparkSession.sparkContext.parallelize(List(1, 2, 3)).repartition(1).count()
+      try {
+        val t = new Thread {
+          override def run(): Unit = {
+            try {
+              sparkSession.sparkContext.parallelize(List(1, 2, 3)).mapPartitions { iter =>
+                Thread.sleep(60 * 1000)
+                iter
+              }.collect()
+            } catch {
+              case _: InterruptedException =>
+            }
+          }
+        }
+        t.start()
 
-    try {
-      val t = new Thread {
-        override def run(): Unit = {
-          try {
-            sparkSession.sparkContext.parallelize(List(1, 2, 3)).mapPartitions { iter =>
-              Thread.sleep(60 * 1000)
-              iter
-            }.collect()
-          } catch {
-            case _: InterruptedException =>
-      t.start()
+        eventually(timeout(20.seconds), interval(100.milliseconds)) {
+          assert(FailedShuffleCleaner.runningStageManager.isRunningStage(2))
+        }
 
-      eventually(timeout(20.seconds), interval(100.milliseconds)) {
-        assert(FailedShuffleCleaner.runningStageManager.isRunningStage(0))
+        sparkSession.sparkContext.cancelAllJobs()
+        t.interrupt()
+
+        eventually(timeout(10.seconds), interval(100.milliseconds)) {
+          assert(!FailedShuffleCleaner.runningStageManager.isRunningStage(2))
+        }
+      } finally {
+        sparkSession.stop()
       }
-
-      sparkSession.sparkContext.cancelAllJobs()
-      t.interrupt()
-
-      eventually(timeout(10.seconds), interval(100.milliseconds)) {
-        assert(!FailedShuffleCleaner.runningStageManager.isRunningStage(0))
-      }
-    } finally {
-      sparkSession.stop()
     }
   }
 }
