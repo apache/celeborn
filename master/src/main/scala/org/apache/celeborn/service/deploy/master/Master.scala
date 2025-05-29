12e91640f68749cabbb4844e2ab7c8b5f254235f
Sanskar Modi <sanskarmodi97@gmail.com>
2025-05-29 11:27:14 +0800

    [CELEBORN-2018] Support min number of workers selected for shuffle
    ### What changes were proposed in this pull request?
    Support min number of workers to assign slots on for a shuffle.
    
    ### Why are the changes needed?
    
    PR https://github.com/apache/celeborn/pull/3039 updated the default value of `celeborn.master.slot.assign.extraSlots` to avoid skew in shuffle stage with less number of reducers. However, it will also affect the stage with large number of reducers, thus not ideal.
    
    We are introducing a new config `celeborn.master.slot.assign.minWorkers` which will ensure that shuffle stages with less number of reducers will not cause load imbalance on few nodes.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes
    
    ### How was this patch tested?
    NA
    
    Closes #3297 from s0nskar/min_workers.
    
    Authored-by: Sanskar Modi <sanskarmodi97@gmail.com>
    Signed-off-by: SteNicholas <programgeek@163.com>

--- master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
+++ master/src/main/scala/org/apache/celeborn/service/deploy/master/Master.scala
@@ -201,6 +201,8 @@ private[celeborn] class Master(
   private val tagsManager = new TagsManager(Option(configService))
 
   private val slotsAssignMaxWorkers = conf.masterSlotAssignMaxWorkers
+  private val slotsAssignMinWorkers = conf.masterSlotAssignMinWorkers
+  private val slotsAssignExtraSlots = conf.masterSlotAssignExtraSlots
   private val slotsAssignLoadAwareDiskGroupNum = conf.masterSlotAssignLoadAwareDiskGroupNum
   private val slotsAssignLoadAwareDiskGroupGradient =
     conf.masterSlotAssignLoadAwareDiskGroupGradient
@@ -974,11 +976,14 @@ private[celeborn] class Master(
         .asScala.map { case (worker, slots) => worker.toUniqueId -> slots }.asJava,
       requestSlots.requestId)
 
-    logInfo(s"Offer slots successfully for $numReducers reducers of $shuffleKey" +
-      s" on ${slots.size()} workers.")
-
+    var offerSlotsMsg = s"Successfully offered slots for $numReducers reducers of $shuffleKey" +
+      s" on ${slots.size()} workers"
     val workersNotSelected = availableWorkers.asScala.filter(!slots.containsKey(_))
-    val offerSlotsExtraSize = Math.min(conf.masterSlotAssignExtraSlots, workersNotSelected.size)
+    val offerSlotsExtraSize = Math.min(
+      Math.max(
+        slotsAssignExtraSlots,
+        slots.size() - slotsAssignMinWorkers),
+      workersNotSelected.size)
     if (offerSlotsExtraSize > 0) {
       var index = Random.nextInt(workersNotSelected.size)
       (1 to offerSlotsExtraSize).foreach(_ => {
@@ -987,8 +992,9 @@ private[celeborn] class Master(
           (new util.ArrayList[PartitionLocation](), new util.ArrayList[PartitionLocation]()))
         index = (index + 1) % workersNotSelected.size
       })
-      logInfo(s"Offered extra $offerSlotsExtraSize slots for $shuffleKey")
+      offerSlotsMsg += s", offered $offerSlotsExtraSize extra slots"
     }
+    logInfo(offerSlotsMsg + ".")
 
     ShuffleAuditLogger.audit(
       shuffleKey,
