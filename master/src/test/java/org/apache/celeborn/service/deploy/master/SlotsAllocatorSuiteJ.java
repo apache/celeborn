0e15397852d144dd641198f31134d53e8eb5131f
Aravind Patnam <akpatnam25@gmail.com>
2025-06-24 15:38:24 -0700

    fix slots calculation bug

--- master/src/test/java/org/apache/celeborn/service/deploy/master/SlotsAllocatorSuiteJ.java
+++ master/src/test/java/org/apache/celeborn/service/deploy/master/SlotsAllocatorSuiteJ.java
@@ -717,7 +717,7 @@ public class SlotsAllocatorSuiteJ {
                               random.nextInt(1000),
                               random.nextInt(1000),
                               0);
-                      diskInfo.maxSlots_$eq(diskInfo.actualUsableSpace() / assumedPartitionSize);
+                      diskInfo.availableSlots_$eq(diskInfo.actualUsableSpace() / assumedPartitionSize);
                       disks.put(diskMountPoint, diskInfo);
                     });
               }
