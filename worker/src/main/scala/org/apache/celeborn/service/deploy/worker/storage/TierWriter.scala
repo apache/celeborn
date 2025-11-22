a8a723b3e13d49e8854b02ec0e55f8c6c52d0fd0
xxx <953396112@qq.com>
2025-11-22 23:02:02 +0800

    [CELEBORN-2211] Avoid allocating additional buffers When HdfsFlushTask writes data

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/storage/TierWriter.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/storage/TierWriter.scala
@@ -26,7 +26,7 @@ import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.JavaConverters.asScalaBufferConverter
 
 import io.netty.buffer.{ByteBuf, CompositeByteBuf}
-import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.{FileSystem, FSDataOutputStream}
 
 import org.apache.celeborn.common.CelebornConf
 import org.apache.celeborn.common.exception.AlreadyClosedException
@@ -522,7 +522,9 @@ class DfsTierWriter(
   private val flushWorkerIndex: Int = flusher.getWorkerIndex
   val hadoopFs: FileSystem = StorageManager.hadoopFs.get(storageType)
   var deleted = false
+  private var hdfsStream: FSDataOutputStream = null
   private var s3MultipartUploadHandler: MultipartUploadHandler = _
+
   private var ossMultipartUploadHandler: MultipartUploadHandler = _
   var partNumber: Int = 1
 
@@ -536,7 +538,7 @@ class DfsTierWriter(
     }
 
   try {
-    hadoopFs.create(dfsFileInfo.getDfsPath, true).close()
+    hdfsStream = hadoopFs.create(dfsFileInfo.getDfsPath, true)
     hadoopFs.setReplication(dfsFileInfo.getDfsPath, conf.workerDfsReplicationFactor.toShort);
     if (dfsFileInfo.isS3) {
       val uri = hadoopFs.getUri
@@ -580,7 +582,7 @@ class DfsTierWriter(
         case ex: InterruptedException =>
           throw new RuntimeException(ex)
       }
-      hadoopFs.create(dfsFileInfo.getDfsPath, true).close()
+      hdfsStream = hadoopFs.create(dfsFileInfo.getDfsPath, true)
   }
 
   storageManager.registerDiskFilePartitionWriter(
@@ -595,7 +597,7 @@ class DfsTierWriter(
   override def genFlushTask(finalFlush: Boolean, keepBuffer: Boolean): FlushTask = {
     notifier.numPendingFlushes.incrementAndGet()
     if (dfsFileInfo.isHdfs) {
-      new HdfsFlushTask(flushBuffer, dfsFileInfo.getDfsPath(), notifier, true, source)
+      new HdfsFlushTask(flushBuffer, hdfsStream, notifier, true, source)
     } else if (dfsFileInfo.isOSS) {
       val flushTask = new OssFlushTask(
         flushBuffer,
@@ -649,6 +651,9 @@ class DfsTierWriter(
   }
 
   override def closeStreams(): Unit = {
+    if (hdfsStream != null) {
+      hdfsStream.close()
+    }
     if (hadoopFs.exists(dfsFileInfo.getDfsPeerWriterSuccessPath)) {
       hadoopFs.delete(dfsFileInfo.getDfsPath, false)
       deleted = true
