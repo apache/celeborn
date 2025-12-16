fc4cb4df54474f592a16e504694952e6bad08c4d
afterincomparableyum <afterincomparableyum>
2025-12-16 04:55:36 -0600

    Add support for celeborn.client.push.maxBytesSizeInFlight in CppClient

--- cpp/celeborn/client/tests/PushStateTest.cpp
+++ cpp/celeborn/client/tests/PushStateTest.cpp
@@ -46,6 +46,45 @@ class PushStateTest : public testing::Test {
   static constexpr int pushTimeoutMs_ = 100;
   static constexpr int pushSleepDeltaMs_ = 10;
   static constexpr int maxReqsInFlight_ = 2;
+  static constexpr int defaultBatchSize_ = 1024;
+};
+
+class PushStateBytesSizeTest : public testing::Test {
+  protected:
+    void SetUp() override {
+      conf::CelebornConf conf;
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushLimitInFlightTimeoutMs,
+          std::to_string(pushTimeoutMs_));
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushLimitInFlightSleepDeltaMs,
+          std::to_string(pushSleepDeltaMs_));
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushMaxReqsInFlightTotal, "100");
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushMaxReqsInFlightPerWorker, "100");
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushMaxBytesSizeInFlightEnabled, "true");
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushMaxBytesSizeInFlightTotal,
+          std::to_string(maxBytesSizeTotal_));
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushMaxBytesSizeInFlightPerWorker,
+          std::to_string(maxBytesSizePerWorker_));
+      conf.registerProperty(
+          conf::CelebornConf::kClientPushBufferMaxSize,
+          std::to_string(bufferMaxSize_));
+
+      pushState_ = std::make_unique<PushState>(conf);
+    }
+
+    std::unique_ptr<PushState> pushState_;
+    static constexpr int pushTimeoutMs_ = 100;
+    static constexpr int pushSleepDeltaMs_ = 10;
+    static constexpr int batchSize_ = 1024;
+    static constexpr long maxBytesSizeTotal_ = 3000;
+    static constexpr long maxBytesSizePerWorker_ = 2500;
+    static constexpr int bufferMaxSize_ = 65536;
 };
 
 TEST_F(PushStateTest, limitMaxInFlight) {
@@ -54,7 +93,7 @@ TEST_F(PushStateTest, limitMaxInFlight) {
   std::vector<bool> addBatchMarks(addBatchCalls, false);
   std::thread addBatchThread([&]() {
     for (auto i = 0; i < addBatchCalls; i++) {
-      pushState_->addBatch(i, hostAndPushPort);
+      pushState_->addBatch(i, defaultBatchSize_, hostAndPushPort);
       EXPECT_FALSE(pushState_->limitMaxInFlight(hostAndPushPort));
       addBatchMarks[i] = true;
     }
@@ -77,7 +116,7 @@ TEST_F(PushStateTest, limitMaxInFlightTimeout) {
   std::vector<bool> addBatchMarks(addBatchCalls, false);
   std::thread addBatchThread([&]() {
     for (auto i = 0; i < addBatchCalls; i++) {
-      pushState_->addBatch(i, hostAndPushPort);
+      pushState_->addBatch(i, defaultBatchSize_, hostAndPushPort);
       auto result = pushState_->limitMaxInFlight(hostAndPushPort);
       if (i < maxReqsInFlight_) {
         EXPECT_FALSE(result);
@@ -103,7 +142,7 @@ TEST_F(PushStateTest, limitZeroInFlight) {
   const int addBatchCalls = 1;
   std::vector<bool> addBatchMarks(addBatchCalls, false);
   std::thread addBatchThread([&]() {
-    pushState_->addBatch(0, hostAndPushPort);
+    pushState_->addBatch(0, defaultBatchSize_, hostAndPushPort);
     EXPECT_FALSE(pushState_->limitZeroInFlight());
     addBatchMarks[0] = true;
   });
@@ -121,7 +160,7 @@ TEST_F(PushStateTest, limitZeroInFlightTimeout) {
   const int addBatchCalls = 1;
   std::vector<bool> addBatchMarks(addBatchCalls, false);
   std::thread addBatchThread([&]() {
-    pushState_->addBatch(0, hostAndPushPort);
+    pushState_->addBatch(0, defaultBatchSize_, hostAndPushPort);
     auto result = pushState_->limitZeroInFlight();
     EXPECT_TRUE(result);
     addBatchMarks[0] = !result;
@@ -152,4 +191,68 @@ TEST_F(PushStateTest, throwException) {
     exceptionThrowed = true;
   }
   EXPECT_TRUE(exceptionThrowed);
+}
+
+TEST_F(PushStateBytesSizeTest, limitMaxInFlightByBytesSize) {
+  const std::string hostAndPushPort = "xx.xx.xx.xx:8080";
+  const int expectedAllowedBatches = 2;
+  const int addBatchCalls = expectedAllowedBatches + 1;
+  std::vector<bool> addBatchMarks(addBatchCalls, false);
+
+  std::thread addBatchThread([&]() {
+    for (auto i = 0; i < addBatchCalls; i++) {
+      pushState_->addBatch(i, batchSize_, hostAndPushPort);
+      auto result = pushState_->limitMaxInFlight(hostAndPushPort);
+      addBatchMarks[i] = true;
+      if (i < expectedAllowedBatches) {
+        EXPECT_FALSE(result) << "Batch " << i << " should be within limits";
+      }
+    }
+  });
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(pushSleepDeltaMs_));
+  for (auto i = 0; i < expectedAllowedBatches; i++) {
+    EXPECT_TRUE(addBatchMarks[i]) << "Batch " << i << " should have completed";
+  }
+
+  pushState_->removeBatch(0, hostAndPushPort);
+  addBatchThread.join();
+  EXPECT_TRUE(addBatchMarks[expectedAllowedBatches]);
+}
+
+TEST_F(PushStateBytesSizeTest, limitMaxInFlightByTotalBytesSize) {
+  const std::string hostAndPushPort1 = "xx.xx.xx.xx:8080";
+  const std::string hostAndPushPort2 = "yy.yy.yy.yy:8080";
+
+  pushState_->addBatch(0, batchSize_, hostAndPushPort1);
+  EXPECT_FALSE(pushState_->limitMaxInFlight(hostAndPushPort1));
+
+  pushState_->addBatch(1, batchSize_, hostAndPushPort2);
+  EXPECT_FALSE(pushState_->limitMaxInFlight(hostAndPushPort2));
+
+  std::atomic<bool> thirdBatchCompleted{false};
+  std::thread addBatchThread([&]() {
+    pushState_->addBatch(2, batchSize_, hostAndPushPort1);
+    pushState_->limitMaxInFlight(hostAndPushPort1);
+    thirdBatchCompleted = true;
+  });
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(pushSleepDeltaMs_));
+  EXPECT_FALSE(thirdBatchCompleted.load())
+      << "Third batch should be blocked due to total bytes limit";
+
+  pushState_->removeBatch(0, hostAndPushPort1);
+  addBatchThread.join();
+
+  EXPECT_TRUE(thirdBatchCompleted.load());
+}
+
+TEST_F(PushStateBytesSizeTest, cleanupClearsBytesSizeTracking) {
+  const std::string hostAndPushPort = "xx.xx.xx.xx:8080";
+
+  pushState_->addBatch(0, batchSize_, hostAndPushPort);
+  pushState_->addBatch(1, batchSize_, hostAndPushPort);
+  pushState_->cleanup();
+
+  EXPECT_FALSE(pushState_->limitMaxInFlight(hostAndPushPort));
 }
