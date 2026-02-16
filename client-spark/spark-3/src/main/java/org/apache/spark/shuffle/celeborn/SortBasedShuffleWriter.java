31811100e4aaa38e91302f86091b918abfdee09d
Enrico Olivelli <eolivelli@gmail.com>
2026-02-16 12:00:03 +0100

    Do not waste resources on hotpath for debug logging on HashBasedShuffleWriter and SortBasedShuffleWriter

--- client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SortBasedShuffleWriter.java
+++ client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SortBasedShuffleWriter.java
@@ -345,7 +345,7 @@ public class SortBasedShuffleWriter<K, V, C> extends ShuffleWriter<K, V> {
   }
 
   private void pushGiantRecord(int partitionId, byte[] buffer, int numBytes) throws IOException {
-    logger.debug("Push giant record, size {}.", Utils.bytesToString(numBytes));
+    if (logger.isDebugEnabled()) logger.debug("Push giant record, size {}.", Utils.bytesToString(numBytes));
     long start = System.nanoTime();
     int bytesWritten =
         shuffleClient.pushData(
