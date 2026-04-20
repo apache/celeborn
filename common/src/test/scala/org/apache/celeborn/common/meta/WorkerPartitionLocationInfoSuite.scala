5ff02d4f4a1fa0119cf88172195168910a0e3e26
SteNicholas <programgeek@163.com>
2026-04-20 20:30:35 +0800

    [CELEBORN-2312] Support committing uncommitted partitions for graceful shutdown

--- common/src/test/scala/org/apache/celeborn/common/meta/WorkerPartitionLocationInfoSuite.scala
+++ common/src/test/scala/org/apache/celeborn/common/meta/WorkerPartitionLocationInfoSuite.scala
@@ -64,6 +64,68 @@ class WorkerPartitionLocationInfoSuite extends CelebornFunSuite {
     assertEquals(workerPartitionLocationInfo.isEmpty, true)
   }
 
+  test("snapshotUncommittedUniqueIds - empty info returns empty maps") {
+    val info = new WorkerPartitionLocationInfo
+    val (primary, replica) = info.snapshotUncommittedUniqueIds
+    assert(primary.isEmpty)
+    assert(replica.isEmpty)
+  }
+
+  test("snapshotUncommittedUniqueIds - captures correct IDs across shuffles") {
+    val info = new WorkerPartitionLocationInfo
+    val shuffle1 = "app1-0"
+    val shuffle2 = "app2-1"
+    val locs1 = new util.ArrayList[PartitionLocation]()
+    locs1.add(mockPartition(0, 0))
+    locs1.add(mockPartition(1, 0))
+    info.addPrimaryPartitions(shuffle1, locs1)
+    val locs2 = new util.ArrayList[PartitionLocation]()
+    locs2.add(mockPartition(2, 0))
+    info.addPrimaryPartitions(shuffle2, locs2)
+    val replicaLocs = new util.ArrayList[PartitionLocation]()
+    replicaLocs.add(mockPartition(3, 0))
+    info.addReplicaPartitions(shuffle1, replicaLocs)
+    val (primary, replica) = info.snapshotUncommittedUniqueIds
+    assert(primary.size == 2)
+    assert(primary(shuffle1).size() == 2)
+    assert(primary(shuffle1).contains("0-0"))
+    assert(primary(shuffle1).contains("1-0"))
+    assert(primary(shuffle2).size() == 1)
+    assert(primary(shuffle2).contains("2-0"))
+    assert(replica.size == 1)
+    assert(replica(shuffle1).size() == 1)
+    assert(replica(shuffle1).contains("3-0"))
+  }
+
+  test("snapshotUncommittedUniqueIds - filters empty shuffle keys") {
+    val info = new WorkerPartitionLocationInfo
+    val shuffleKey = "app1-0"
+    val locs = new util.ArrayList[PartitionLocation]()
+    locs.add(mockPartition(0, 0))
+    locs.add(mockPartition(1, 0))
+    info.addPrimaryPartitions(shuffleKey, locs)
+    info.removePrimaryPartitions(shuffleKey, locs.asScala.map(_.getUniqueId).asJava)
+    val (primary, _) = info.snapshotUncommittedUniqueIds
+    assert(!primary.contains(shuffleKey))
+  }
+
+  test("snapshotUncommittedUniqueIds - snapshot is a point-in-time copy") {
+    val info = new WorkerPartitionLocationInfo
+    val shuffleKey = "app1-0"
+    val locs = new util.ArrayList[PartitionLocation]()
+    locs.add(mockPartition(0, 0))
+    info.addPrimaryPartitions(shuffleKey, locs)
+    val (primary, _) = info.snapshotUncommittedUniqueIds
+    assert(primary(shuffleKey).size() == 1)
+    // Add more partitions after snapshot
+    val moreLocs = new util.ArrayList[PartitionLocation]()
+    moreLocs.add(mockPartition(1, 0))
+    moreLocs.add(mockPartition(2, 0))
+    info.addPrimaryPartitions(shuffleKey, moreLocs)
+    // Snapshot remains unchanged
+    assert(primary(shuffleKey).size() == 1)
+  }
+
   private def mockPartition(partitionId: Int, epoch: Int): PartitionLocation = {
     new PartitionLocation(
       partitionId,
